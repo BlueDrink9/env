ecedcf3f48a5673258177a602f949fd1b184142a
will-shaw <will_shaw@live.com>
2017-09-18 13:34:38 +1200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ SKIP='0'
 
 echo -ne Detecting OS...
 
-if [ $OSTYPE == 'linux-gnu' ]; then
+if [[ $OSTYPE == 'linux-gnu' ]]; then
     echo -e "[\e[32mLinux\e[0m]"
 
     echo -ne "Creating backups..."
-    if [ -d "$BAKDIR" ]; then
+    if [[ -d "$BAKDIR" ]]; then
         echo -ne "existing backup detected.\n\e[33mContinue? (y/n) \e[0m"
         read -n 1 REPLY
         echo ""
@@ -23,20 +23,20 @@ if [ $OSTYPE == 'linux-gnu' ]; then
         mkdir "$BAKDIR"
     fi
 
-    if [ -f "$BAKDIR/.version" ]; then
+    if [[ -f "$BAKDIR/.version" ]]; then
         echo -ne "Checking existing version..."
         EXISTING=$(< "$BAKDIR/.version")
-        if [ "$EXISTING" == "$VERSION" ]; then
+        if [[ "$EXISTING" == "$VERSION" ]]; then
             echo -e "Detected [\e[32m$VERSION\e[0m] Skipping to check Vim."
             SKIP='2'
         else
             out="cat $BAKDIR/.version"
-            echo $out   
+            echo $out
             echo -e "Detected [\e[31m$EXISTING\e[0m] Updating => [\e[32m$VERSION\e[0m]"
         fi
     fi
 
-    if [ ! "$SKIP" == '2' ]; then
+    if [[ ! "$SKIP" == '2' ]]; then
         echo "$VERSION" > "$BAKDIR/.version"
 
         echo -ne "Backing up existing config files to \e[36mfile://$BAKDIR\e[0m..."
@@ -57,11 +57,11 @@ if [ $OSTYPE == 'linux-gnu' ]; then
 
     echo -ne "Checking Vim..."
 
-    if [ -d "$VIMDIR" ]; then
+    if [[ -d "$VIMDIR" ]]; then
         echo -ne "found custom Vim.\nUpdating => "
 
         cd "$VIMDIR"
-        git rebase | xargs echo -n 
+        git rebase | xargs echo -n
         cd "$WD"
         echo -e " [\e[32mOK\e[0m]"
     else
@@ -69,12 +69,12 @@ if [ $OSTYPE == 'linux-gnu' ]; then
 	    git clone --depth=1 https://github.com/amix/vimrc.git "$VIMDIR"
     fi
     sh "$VIMDIR/install_awesome_vimrc.sh" 2> /dev/null
-    
-    echo -e "[\e[32mInstall Complete\e[0m]" 
 
-elif [ $OSTYPE == 'darwin' ]; then
+    echo -e "[\e[32mInstall Complete\e[0m]"
+
+elif [[ $OSTYPE == 'darwin' ]]; then
     echo You are running Mac
-elif [ $OSTYPE == 'msys' ]; then
+elif [[ $OSTYPE == 'msys' ]]; then
         echo You are using Git Bash on Windows
 else
     echo "OS not set... Exiting with no change."
