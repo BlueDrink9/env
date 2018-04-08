4c9185d720ed6b4f1e1807679543f85a0e3d0b99
BlueDrink9 <bluedrink9@gmail.com>
2018-04-08 19:16:45 +1200

    Add ls alias for colour

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -13,6 +13,7 @@ if [[ "$OSTYPE" =~ "darwin1" ]]; then  # OSX specific stuff
     # Make esc act as backspace in terminal
 
 elif [ "$OSTYPE" = "linux-gnu" ]; then  # Linux specific stuff
+    alias ls="ls --color=auto"
     # enable color support of ls and also add handy aliases
     alias dir='dir --color=auto'
     alias vdir='vdir --color=auto'
