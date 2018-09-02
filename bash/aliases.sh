585139ff7c3e44abf3c68a067ffad7552eddd023
bluedrink9 <bluedrink9@gmail.com>
2018-09-02 13:38:21 +1200

    Misc small

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -91,3 +91,6 @@ alias sagi="sudo apt install"
 alias sag="sudo apt"
 alias sagu="sudo apt update && sudo apt upgrade"
 
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
