9cfa4a62c80944862000ce32547f56ed57dd9289
bluedrink9 <>
2019-01-11 15:31:24 +1300

    Fix termux package installation and setup

--- terminal/termux/setup.sh
+++ terminal/termux/setup.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 # Allow launching of apps by name?
 mkdir -p "$HOME/.termux"
-downloadURLAndExtractZipTo "https://github.com/adi1090x/termux-style/raw/master/data.tar.gz" "$HOME/.termux/termux-style"
-cp "$HOME/.termux/termux-style/solarized-light.properties" "$HOME/.termux/"
+downloadURLAndExtractZipTo "https://github.com/adi1090x/termux-style/raw/master/data.tar.gz" \
+    "$HOME/.termux/termux-style" && \
+    cp "$HOME/.termux/termux-style/solarized-light.properties" "$HOME/.termux/"
 # Provide launcher support. Launch _
 curl https://github.com/thewisenerd/dotfiles/blob/88d0534c67c2e93f601b1dd757cea9072eda6827/bin/zx -o /usr/bin/launch
 chmod +x /usr/bin/launch
-for file in packages; do
-    pkg install $file
-done
+while read -r pkg; do
+# for file in $(cat $($SCRIPTDIR_CMD)/packages); do
+    pkg install $pkg
+done < "$($SCRIPTDIR_CMD)/packages"
+termux-setup-api
+# Disable openssh password auth. Only allow public key.
+sed -i 's/#PasswordAuthentication.*/PasswordAuthentication no/' $PREFIX/etc/ssh/sshd_config
