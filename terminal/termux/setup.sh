57705caecd2ebc331a8806506998dc4fb9e2ed82
bluedrink9 <>
2019-01-28 12:14:13 +1300

    Set up termux editor

--- terminal/termux/setup.sh
+++ terminal/termux/setup.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
-# Allow launching of apps by name?
+source "$DOTFILES_DIR/bash/script_functions.sh"
+
 mkdir -p "$HOME/.termux"
 downloadURLAndExtractZipTo "https://github.com/adi1090x/termux-style/raw/master/data.tar.gz" \
     "$HOME/.termux/termux-style" && \
     cp "$HOME/.termux/termux-style/solarized-light.properties" "$HOME/.termux/"
+
+# Allow launching of apps by name?
 # Provide launcher support. Launch _
 curl https://github.com/thewisenerd/dotfiles/blob/88d0534c67c2e93f601b1dd757cea9072eda6827/bin/zx -o /usr/bin/launch
 chmod +x /usr/bin/launch
-# while read -r pkg; do
-# # for file in $(cat $($SCRIPTDIR_CMD)/packages); do
-#     pkg install $pkg
-# done < "$($SCRIPTDIR_CMD)/packages"
 
 # Disable openssh password auth. Only allow public key.
 sed -i 's/#PasswordAuthentication.*/PasswordAuthentication no/' $PREFIX/etc/ssh/sshd_config
+
+mkdir -p $HOME/.shortcuts
+ln "$HOME/.shortcuts/vim" "$($SCRIPTDIR_CMD)/vim"
+mkdir -p $HOME/bin
+ln "$HOME/termux-file-editor" "$HOME/.shortcuts/vim"
 
 echo Check https://android.stackexchange.com/questions/37/how-do-i-change-the-name-of-my-android-device to alter the hostname
