9eaf9b4fc91cff52a641d11cc69879b9817d2a25
bluedrink9 <bluedrink9@gmail.com>
2019-03-09 12:35:24 +1300

    Change shkd/chunkwm to use .load for configs

--- windowManagers/chunkwm/install.sh
+++ windowManagers/chunkwm/install.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 source "$DOTFILES_DIR/bash/script_functions.sh"
 installID="Chunkwm"
-installText="chunkc core::plugin_dir $HOMEBREW_PREFIX/opt/chunkwm/share/chunkwm/plugins; source \"$($SCRIPTDIR_CMD)/chunkwmrc\""
 configDir="${HOME}"
+installText="chunkc core::plugin_dir $HOMEBREW_PREFIX/opt/chunkwm/share/chunkwm/plugins; source \"$($SCRIPTDIR_CMD)/chunkwmrc\""
 baseRC="${configDir}/.chunkwmrc"
 
 eval "$(cat <<END
 do${installID}() {
     printErr "Enabling chunkwm & skhd config..."
     addTextIfAbsent "${installText}" "${baseRC}"
+    doSkhd
     getFloatScripts
-}
+  }
 END
 )"
 getFloatScripts() {
@@ -18,14 +19,20 @@ getFloatScripts() {
   # downloadURLtoFile https://github.com/Amar1729/dotfiles/blob/407803a1de201dc63c57a9d4ee5bc23c09bcdb20/.config/scripts/chunk_resize_mgr.sh chunk_resize_mgr.sh
 }
 
+doSkhd(){
+  installText=".load \"$($SCRIPTDIR_CMD)/skhd/skhdrc\""
+  baseRC="$HOME/.skhdrc"
+  addTextIfAbsent "${installText}" "${baseRC}"
+}
+
 eval "$(cat <<END
 undo${installID}(){
     mv "${baseRC}" "${baseRC}.pre-env"
-}
+  }
 END
 )"
 
 # If directly run instead of sourced, do all
 if [ ! "${BASH_SOURCE[0]}" != "${0}" ]; then
-    do${installID}
+do${installID}
 fi
