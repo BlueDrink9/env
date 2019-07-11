e3f427738971d22bd904b02e73ac0e47b156563c
bluedrink9 <bluedrink9@github.com>
2019-07-11 22:34:18 +1200

    Float AXDialog and AXUnknown type windows

--- windowManagers/chunkwm/rules.sh
+++ windowManagers/chunkwm/rules.sh
@@ -11,6 +11,10 @@ chunkc tiling::rule --owner \"OneNote*\" --state tile &
 chunkc tiling::rule --owner \"PowerPoint*\" --state tile &
 chunkc tiling::rule --owner \"Excel*\" --state tile &
 chunkc tiling::rule --owner PowerPoint --state tile &
+chunkc tiling::rule --owner PowerPoint --subrole AXUnknown --state float &
+chunkc tiling::rule --owner PowerPoint --subrole AXDialog --state float &
+chunkc tiling::rule --subrole AXUnknown --state float &
+chunkc tiling::rule --subrole AXDialog --state float &
 chunkc tiling::rule --owner Excel --state tile &
 chunkc tiling::rule --owner Word --state tile &
 chunkc tiling::rule --owner OneNote --state tile &
