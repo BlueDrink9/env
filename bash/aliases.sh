f1a05dcb746f296bdfabdaaa5e115c4c99109eb9
bluedrink9 <bluedrink9@github.com>
2019-07-18 13:35:56 +1200

    reloadConfig alias (pkill -usr1

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -99,3 +99,5 @@ alias svi="sudoedit"
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+alias reloadConfig="pkill -USR1"
