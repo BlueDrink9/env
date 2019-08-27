55aeab0d428b193e80c71d7ff671c9356787b1cb
bluedrink9 <bluedrink9@github.com>
2019-08-27 12:54:38 +1200

    Fix xdm class

--- windowManagers/bspwm/rules.sh
+++ windowManagers/bspwm/rules.sh
@@ -3,7 +3,9 @@ bspc rule -a Skype pseudo_tiled=on
 # bspc rule -a Spotify pseudo_tiled=on
 # bspc rule -a Thunar pseudo_tiled=on
 bspc rule -a Chromium desktop='^2'
-bspc rule -a xdm state=floating
+# bspc rule -a xdm state=floating
+# This is the actual xdm WM_CLASS.
+bspc rule -a java-lang-Thread state=floating
 bspc rule -a mplayer2 state=floating
 bspc rule -a Kupfer.py focus=on
 bspc rule -a Screenkey manage=off floating=on
