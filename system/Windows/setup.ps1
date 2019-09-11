bb797724dc0e15f41f03a1f0934c153e493b9e75
BlueDrink9 <BlueDrink9@users.noreply.github.com>
2019-09-11 16:31:20 +1200

    Create setup.ps1

---
+++ system/Windows/setup.ps1
@@ -0,0 +1,15 @@
+# To be run from administrative shell
+Set-ExecutionPolicy Unrestricted -Force
+
+# Install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco feature enable -n=allowGlobalConfirmation
+
+CINST Boxstarter -y
+Import-Module Boxstarter.Chocolatey
+$Boxstarter.RebootOk=$true
+$Boxstarter.NoPassword=$false
+$Boxstarter.AutoLogin=$true
+$cred=Get-Credential domain\username
+Install-BoxstarterPackage -PackageName "boxstarter script.ps1" -Credential $cred
+Install-BoxstarterPackage -PackageName "packages.ps1" -Credential $cred
