adcac518af222ee657f8b5eb1b3a67e89cecb3d5
bluedrink9 <bluedrink9@github.com>
2019-09-12 11:19:46 +1200

    prompt: Only show hostname if sshsession

--- bash/prompt/custom_prompt.sh
+++ bash/prompt/custom_prompt.sh
@@ -35,14 +35,18 @@ set_bash_prompt () {
   USER_COLOURED="${pblue}\u${pNC}"
   USER_INITIAL_COLOURED="${pblue}${USER:0:1}${pNC}"
 
-  export HOST=$(uname -n | cut -d"." -f1)
+  if [ -n "$SSHSESSION" ]; then
+    export HOST=$(uname -n | cut -d"." -f1)
 
-  if (( ${#HOST}  > 12 )); then
-    # Truncate hostname if it is too long.
-    HOST=${HOST:0:12}
-    HOST_COLOURED="${pyellow}${HOST}${pNC}"
+    if (( ${#HOST}  > 12 )); then
+      # Truncate hostname if it is too long.
+      HOST=${HOST:0:12}
+      HOST_COLOURED="${pNC}@${pyellow}${HOST}${pNC}"
+    else
+      HOST_COLOURED="${pNC}@${pyellow}\h${pNC}"
+    fi
   else
-    HOST_COLOURED="${pyellow}\h${pNC}"
+    HOST=""
   fi
 
   CURR_FULL_PATH="\w"
@@ -81,11 +85,11 @@ set_bash_prompt () {
   VAR_PROMPT=""
 
   if (( $((${COLUMNS} - $prompt_len)) > ${DESIRED_COMMAND_SPACE})); then
-    VAR_PROMPT="${TIME_PROMPT_COLOURED} ${USER_COLOURED}@${HOST_COLOURED}:"
+    VAR_PROMPT="${TIME_PROMPT_COLOURED} ${USER_COLOURED}${HOST_COLOURED}:"
   elif (( $((${COLUMNS} - $prompt_len_no_time)) > ${DESIRED_COMMAND_SPACE})); then
-    VAR_PROMPT="${USER_COLOURED}@${HOST_COLOURED}:"
+    VAR_PROMPT="${USER_COLOURED}${HOST_COLOURED}:"
   elif (( $((${COLUMNS} - $prompt_len_no_time_host)) > ${DESIRED_COMMAND_SPACE})); then
-    VAR_PROMPT="${USER_INITIAL_COLOURED}@${HOST_COLOURED}:"
+    VAR_PROMPT="${USER_INITIAL_COLOURED}${HOST_COLOURED}:"
     # elif (( $((${COLUMNS} - $prompt_len_no_time_host)) > ${DESIRED_COMMAND_SPACE})); then
     #   VAR_PROMPT="${USER_COLOURED}: "
     # let "remaining_space= ${COLUMNS} - $prompt_len_no_time_host_user"
