1338c6aa38a5fa56385c74bb007f1cd00fbb71ae
BlueDrink9 <bluedrink9@github.com>
2019-09-12 12:48:26 +1200

    Update working dir of ps1

--- system/Windows/setup.ps1
+++ system/Windows/setup.ps1
@@ -1,7 +1,12 @@
 # To be run from administrative shell
-#Requires -RunAsAdministrator
+##Requires -RunAsAdministrator
 Set-ExecutionPolicy Unrestricted -Force
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+$scriptpath = $MyInvocation.MyCommand.Path
+$scriptdir = Split-Path $scriptpath
+cd $scriptdir
+[Environment]::CurrentDirectory = $PWD
 
 # Install chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
@@ -13,5 +18,5 @@ $Boxstarter.RebootOk=$true
 $Boxstarter.NoPassword=$false
 $Boxstarter.AutoLogin=$true
 $cred=Get-Credential domain\username
-Install-BoxstarterPackage -PackageName ".\boxstarter script.ps1" -Credential $cred
-Install-BoxstarterPackage -PackageName ".\packages.ps1" -Credential $cred
+Install-BoxstarterPackage -PackageName Resolve-Path "boxstarter script.ps1" -Credential $cred
+Install-BoxstarterPackage -PackageName Resolve-Path "packages.ps1" -Credential $cred
