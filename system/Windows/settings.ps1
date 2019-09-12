733c91166a4a351bb574e3a582b2008caf856f10
BlueDrink9 <bluedrink9@github.com>
2019-09-12 18:34:27 +1200

    Windows: Add options

--- system/Windows/settings.ps1
+++ system/Windows/settings.ps1
@@ -96,6 +96,55 @@ Set-ItemProperty -Path "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\
 # No SMB1 - https://blogs.technet.microsoft.com/filecab/2016/09/16/stop-using-smb1/
 Disable-WindowsOptionalFeature -Online -FeatureName smb1protocol
 
+# Privacy settings.
+# . Logit "Disabling 'Send Microsoft info about how I write to help us improving typing and writing in the future' functionality..."
+New-Item -Path 'HKCU:\SOFTWARE\Microsoft\Input' -Name 'TIPC' -Force
+New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Input\TIPC" -Name "Enabled" -PropertyType DWORD -Value "0" -Force
+# . Logit "Adjusting feedback frequency. A value of 0 indicates that user is never prompted to provide feedback about Windows 10 functionality by Microsoft."
+New-Item -Path 'HKCU:\Software\Microsoft\Siuf' -Name 'Rules' -Force
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Siuf\Rules" -Name "PeriodInNanoSeconds" -PropertyType DWORD -Value "0" -Force
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Siuf\Rules" -Name "NumberOfSIUFInPeriod" -PropertyType DWORD -Value "0" -Force
+
+#################
+# Services
+#################
+
+# . Logit "Disabling Microsoft Account Sign-in Assistant Service..."
+Set-Service wlidsvc -StartupType Disabled
+# . Logit "Disabling Windows Error Reporting Service..."
+Set-Service WerSvc -StartupType Disabled
+# . Logit "Disabling Xbox Live Auth Manager Service..."
+Set-Service XblAuthManager -StartupType Disabled
+# . Logit "Disabling Xbox Live Game Save Service..."
+Set-Service XblGameSave -StartupType Disabled
+# . Logit "Disabling Xbox Live Networking Service Service..."
+Set-Service XboxNetApiSvc -StartupType Disabled
+# . Logit "Disabling Xbox Accessory Management Service..."
+# Set-Service XboxGipSvc -StartupType Disabled
+
+#################
+# Sched tasks
+#################
+# . Logit "Disabling Scheduled Tasks..."
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Application Experience\ProgramDataUpdater"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Application Experience\StartupAppTask"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Customer Experience Improvement Program\Consolidator"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Customer Experience Improvement Program\UsbCeip"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticResolver"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Maps\MapsToastTask"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Maps\MapsUpdateTask"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Shell\FamilySafetyMonitor"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\WDI\ResolutionHost"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Windows Media Sharing\UpdateLibrary"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Autochk\Proxy"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\CloudExperienceHost\CreateObjectTask"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Feedback\Siuf\DmClient"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Feedback\Siuf\DmClientOnScenarioDownload"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Shell\FamilySafetyRefreshTask"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Windows Error Reporting\QueueReporting"
+Disable-ScheduledTask -TaskName "\Microsoft\XblGameSave\XblGameSaveTask"
+
 #################
 # Windows Updates
 #################
