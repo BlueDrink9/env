7d3e577574d0d831e194705ba21aa18649971779
BlueDrink9 <bluedrink9@github.com>
2019-09-12 21:08:37 +1200

    Fix

--- system/Windows/boxstarter-main.ps1
+++ system/Windows/boxstarter-main.ps1
@@ -1,5 +1,5 @@
 # $scriptdir = $PSScriptRoot
-$scriptdir get-content -path $TMP\dotfiles_win_setup_dir.txt
+$scriptdir = get-content -path $env:TEMP\dotfiles_win_setup_dir.txt
 . "$scriptdir\packages.ps1"
 . "$scriptdir\remove-default-apps.ps1"
 . "$scriptdir\settings.ps1"
