ef2db8c342594ddd567783e95461eb66677a736f
BlueDrink9 <bluedrink9@github.com>
2019-09-12 12:54:02 +1200

    Update path

--- system/Windows/setup.ps1
+++ system/Windows/setup.ps1
@@ -1,10 +1,15 @@
+# Run this line manually beforehand to allow this script to run, which will
+# elevate itself then handle the rest.
+# Set-ExecutionPolicy -scope currentuser Unrestricted -Force
+#
 # To be run from administrative shell
 ##Requires -RunAsAdministrator
 Set-ExecutionPolicy Unrestricted -Force
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
-$scriptpath = $MyInvocation.MyCommand.Path
-$scriptdir = Split-Path $scriptpath
+# $scriptpath = $MyInvocation.MyCommand.Path
+# $scriptdir = Split-Path $scriptpath
+$scriptdir = $PSScriptRoot
 cd $scriptdir
 [Environment]::CurrentDirectory = $PWD
 
