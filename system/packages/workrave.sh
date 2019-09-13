ed62ec4aaafd38bd23bd166b868b52b02e07fd55
BlueDrink9 <bluedrink9@github.com>
2019-09-14 11:30:35 +1200

    Fixes to workrave script

--- system/packages/workrave.sh
+++ system/packages/workrave.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 pushd $HOME
 mkdir -p ~/Applications ~/.local/build ~/.local/src
-rm -rf ~/Library/Preferences/com.workrave.plist
-rm -rf ~/Library/Preferences/workrave.plist
+# rm -rf ~/Library/Preferences/com.workrave.plist
+# rm -rf ~/Library/Preferences/workrave.plist
 # Officially required packages:
 wr_dependencies_official="gettext intltool gobject-introspection autoconf-archive gtk+ gtk-mac-integration gtkmm3"
 # These packages get it working without crashes, with caveat of no symbols/icons. IDK which one does the trick.
@@ -10,7 +10,7 @@ wr_dependencies_extra="automake autoconf libtool gnome-icon-theme hicolor-icon-t
 brew install $wr_dependencies_official $wr_dependencies_extra
 brew link --force gettext libffi
 git clone https://github.com/palfrey/workrave/ "$HOME/.local/src/workrave"
-cd workrave
+cd .local/src/workrave
 git checkout os-x-fixes
 autoreconf -if
 intltoolize --automake --copy --force
