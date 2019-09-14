4b05688261ce911479a319ddc58d73d8c1b649f0
BlueDrink9 <bluedrink9@github.com>
2019-09-15 10:45:36 +1200

    Workrave: Use 'packages/' instead of 'build/'

--- system/packages/workrave.sh
+++ system/packages/workrave.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 pushd $HOME
-mkdir -p ~/Applications ~/.local/build ~/.local/src
-rm -rf ~/Library/Preferences/com.workrave.plist
-rm -rf ~/Library/Preferences/workrave.plist
+mkdir -p ~/Applications ~/.local/packages ~/.local/src
+# rm -rf ~/Library/Preferences/com.workrave.plist
+# rm -rf ~/Library/Preferences/workrave.plist
 # Officially required packages:
 wr_dependencies_official="gettext intltool gobject-introspection autoconf-archive gtk+ gtk-mac-integration gtkmm3"
 # These packages get it working without crashes, with caveat of no symbols/icons. IDK which one does the trick.
@@ -10,12 +10,12 @@ wr_dependencies_extra="automake autoconf libtool gnome-icon-theme hicolor-icon-t
 brew install $wr_dependencies_official $wr_dependencies_extra
 brew link --force gettext libffi
 git clone https://github.com/palfrey/workrave/ "$HOME/.local/src/workrave"
-cd workrave
+cd .local/src/workrave
 git checkout os-x-fixes
 autoreconf -if
 intltoolize --automake --copy --force
-./configure --prefix="$HOME/.local/build/workrave" --config-cache
+./configure --prefix="$HOME/.local/packages/workrave" --config-cache
 make
 make install
-stow  --dir="$HOME/.local/build" workrave --target="$HOME/.local"
+stow  --dir="$HOME/.local/packages" workrave --target="$HOME/.local"
 popd
