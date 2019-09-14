635583e80b6b9d8d98b2a7d8eb435e43b179fca7
bluedrink9 <bluedrink9@users.noreply.github.com>
2019-09-14 12:25:11 +1200

    Windows: Settings for powerbutton and lid

--- system/Windows/settings.ps1
+++ system/Windows/settings.ps1
@@ -187,6 +187,26 @@ powercfg /change standby-timeout-ac 0
 powercfg /change monitor-timeout-dc 5
 # Sleep on Battery (Laptop Only)
 powercfg /change standby-timeout-dc 10
+# powercfg /setacvalueindex 
+# Get active scheme guid
+$activeScheme = cmd /c "powercfg /getactivescheme"
+$regEx = '(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}'
+$asGuid = [regex]::Match($activeScheme,$regEx).Value
+$asGuid
+#relative GUIDs for Lid Close settings
+$pwrGuid = '4f971e89-eebd-4455-a8de-9e59040e7347'
+$lidClosedGuid = '5ca83367-6e45-459f-a27b-476b1d01c936'
+# DC Value // On Battery // 1 = sleep
+cmd /c "powercfg /setdcvalueindex $asGuid $pwrGuid $lidClosedGuid 1"
+#AC Value // While plugged in // 0 = do nothing
+cmd /c "powercfg /setacvalueindex $asGuid $pwrGuid $lidClosedGuid 0"
+$buttonGuid = '7648efa3-dd9c-4e3e-b566-50f929386280'
+# DC Value // On Battery // 1 = sleep
+cmd /c "powercfg /setdcvalueindex $asGuid $pwrGuid $buttonGuid 1"
+#AC Value // While plugged in // 3 = shut down
+cmd /c "powercfg /setacvalueindex $asGuid $pwrGuid $buttonGuid 3"
+#apply settings
+cmd /c "powercfg /s $asGuid"
 
 #--- Rename the Computer ---
 # Requires restart, or add the -Restart flag
