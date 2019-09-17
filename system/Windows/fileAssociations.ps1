bd5dccf6185f61317c9b57d9367e985c0482a8e9
bluedrink9 <bluedrink9@users.noreply.github.com>
2019-09-18 00:43:38 +1200

    Fix association bug

--- system/Windows/fileAssociations.ps1
+++ system/Windows/fileAssociations.ps1
@@ -59,13 +59,13 @@ $vimbin = "$(where gvim.exe)"
 if ([string]::IsNullOrEmpty($vimbin)) {
     $vimbin = 'C:\Program Files (x86)\Vim\vim80\gvim.exe'
 }
-
-cmd /c "ftype sourcecode=`"$vimbin`" `"%1`""
-if (-not (test-path "HKCU:\plaintextRunnable\Shell\Edit\command")) {
-    New-Item -Path "HKCU:\plaintextRunnable"
-    New-Item -Path "HKCU:\plaintextRunnable\Shell"
-    New-Item -Path "HKCU:\plaintextRunnable\Shell\Edit"
-    New-Item -Path "HKCU:\plaintextRunnable\Shell\Edit\command"
+cmd /c "ftype plaintext=$vimbin `"%1`""
+cmd /c "ftype text=$vimbin `"%1`""
+if (-not (test-path "HKCU:\Software\Classes\plaintextRunnable\Shell\Edit\command")) {
+    New-Item -Path "HKCU:\Software\Classes\plaintextRunnable"
+    New-Item -Path "HKCU:\Software\Classes\plaintextRunnable\Shell"
+    New-Item -Path "HKCU:\Software\Classes\plaintextRunnable\Shell\Edit"
+    New-Item -Path "HKCU:\Software\Classes\plaintextRunnable\Shell\Edit\command"
 }
 # Set-item alone sets default.
-New-ItemProperty -Path "HKCU:\plaintextRunnable\Shell\Edit\command" -Name "(Default)" -PropertyType String -Value "`"$vimbin`" `"%1`""
+New-ItemProperty -Path "HKCU:\Software\Classes\plaintextRunnable\Shell\Edit\command" -Name "(Default)" -PropertyType String -Value "`"$vimbin`" `"%1`""
