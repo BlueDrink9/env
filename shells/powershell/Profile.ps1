b23746358a451bb78abf141b8f19b8538522527c
BlueDrink9 <bluedrink9@github.com>
2019-09-24 20:09:33 +1200

    powershell suggestions

--- shells/powershell/Profile.ps1
+++ shells/powershell/Profile.ps1
@@ -1,4 +1,8 @@
 # Check out https://github.com/cspotcode/PS-GuiCompletion
+# Check out https://github.com/joonro/Get-ChildItem-Color
+# https://www.powershellgallery.com/packages/oh-my-posh/2.0.225
+# console setting tool. Easy solarized!
+# https://github.com/lukesampson/concfg
 # Complete to LCS?
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 # Alternative, by default mapped to c-space
