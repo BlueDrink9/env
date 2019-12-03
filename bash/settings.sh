28cb341ee89b32444e6adb20b3c69e898374cec1
bluedrink9 <bluedrink9@github.com>
2019-12-03 15:06:38 +1300

    Increase HISTFILESIZE

--- bash/settings.sh
+++ bash/settings.sh
@@ -53,7 +53,7 @@ fi
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 export HISTSIZE=1000
-export HISTFILESIZE=2000
+export HISTFILESIZE=5000
 # don't put duplicate lines or lines starting with space in the history.
 # See bash(1) for more options
 # export HISTCONTROL=ignoreboth:erasedups
