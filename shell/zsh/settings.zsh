bdd701b0aed702f044a5a6a6ecf57119a28773da
bluedrink9 <bluedrink9@github.com>
2019-12-03 18:53:34 +1300

    set noflowcontrol

--- shell/zsh/settings.zsh
+++ shell/zsh/settings.zsh
@@ -5,6 +5,8 @@ setopt no_clobber
 HISTFILE=~/.zsh_history
 setopt appendhistory autocd nomatch
 unsetopt beep notify
+# Disable ctrl+s for stopping flow.
+setopt noflowcontrol
 # Allows ? to be used in commands. Otherwise is a glob meaning 'any char'.
 # But escaping the ? works in both bash and zsh.
 # unsetopt nomatch
