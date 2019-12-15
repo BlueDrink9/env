8cf786f9b568324d1a627c1ca6911c8e5f81ae8a
bluedrink9 <bluedrink9@github.com>
2019-12-15 14:16:23 +1300

    Fonts: Update font_install, install meslo

--- font_install.sh
+++ font_install.sh
@@ -14,31 +14,35 @@ doFonts() {
     fi
 
     printErr "Downloading fonts..."
-
     # # Get latest Iosevka font release.
     # fontUrl=$(getLatestReleaseFileURL "be5invis/Iosevka" "iosevka-pack-[^z]*zip")
-    # fontdir="${FONTDIR}/Iosevka"
-    # downloadURLAndExtractZipTo $fontUrl $fontdir && \
-    #     printErr "${OK} Fonts installed to ${Yellow}${fontdir}${NC}" || \
-    #     printErr "${Error} ${Red}Fonts failed to install to ${Yellow}${fontdir}${NC}"
 
-    printErr "Downloading Sauce Code Pro..."
-    SCPUrl=$(getLatestReleaseFileURL "ryanoasis/nerd-fonts" "SourceCodePro\.zip")
-    SCPdir="${FONTDIR}/SauceCodeProNF"
-    downloadURLAndExtractZipTo "$SCPUrl" "$SCPdir" && \
-        printErr "${OK} Fonts installed to ${Yellow}${SCPdir}${NC}" || \
-        printErr "${Error} ${Red}Fonts failed to install to ${Yellow}${SCPdir}${NC}"
+    downloadFont "mesloNF" "ryanoasis/nerd-fonts" "Meslo\.zip"
+    downloadFont "SauceCodeProNF" "ryanoasis/nerd-fonts" "SourceCodePro\.zip"
 
-    if [[ $OSTYPE == 'linux-gnu' ]]; then
-        # Unused mac-required SCP fonts.
-        rm -f "${SCPdir}/*Windows Compatible.ttf"
-    elif [[ $OSTYPE =~ 'darwin' ]]; then
-        rm -f "${SCPdir}/*Complete.ttf"
-        rm -f "${SCPdir}/*Mono.ttf"
-    fi
+    # if [[ $OSTYPE == 'linux-gnu' ]]; then
+    #     # Unused mac-required SCP fonts.
+    #     rm -f "${SCPdir}/*Windows Compatible.ttf"
+    # elif [[ $OSTYPE =~ 'darwin' ]]; then
+    #     rm -f "${SCPdir}/*Complete.ttf"
+    #     rm -f "${SCPdir}/*Mono.ttf"
+    # fi
 
     fc-cache && printErr "${OK} Fontcache updated" || \
         printErr "${Error} ${Red}Failed to update fontcache${NC}"
+}
+
+downloadFont(){
+    name="$1"
+    repo="$2"
+    file="$3"
+    printErr "Downloading ${name}..."
+    fontUrl=$(getLatestReleaseFileURL "${repo}" "${file}")
+    fontdir="${FONTDIR}/${name}"
+    downloadURLAndExtractZipTo "${fontUrl}" "${fontdir}" && \
+        printErr "${OK} ${name} installed to ${Yellow}${fontdir}${NC}" || \
+        printErr "${Error} ${Red} ${name} failed to install to ${Yellow}${fontdir}${NC}"
+
 }
 
 undoFonts(){
