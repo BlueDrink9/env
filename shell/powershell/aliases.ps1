40fa291a02530ca7a1f4920e7ad83a894fe0633a
bluedrink9 <bluedrink9@users.noreply.github.com>
2020-01-26 00:30:45 +1300

    Update editor aliases

--- shell/powershell/aliases.ps1
+++ shell/powershell/aliases.ps1
@@ -1,6 +1,11 @@
+# Aliases to programs with arguments must be created as functions. Consider just using functions instead.
 # Set-Alias ;q exit
 Set-Alias ';q' exit
+# Set-Alias where where.exe
+Set-Alias which where.exe
 Set-Alias g git
 Set-Alias e nvim
-Set-Alias le 'nvim --cmd "let g:liteMode=1"'
-Set-Alias ide 'nvim --cmd "let g:ideMode=1"'
+function liteEdit {nvim --cmd "let g:liteMode=1"}
+Set-Alias le liteEdit
+function ide {nvim --cmd "let g:ideMode=1"}
+Set-Alias ide ide
