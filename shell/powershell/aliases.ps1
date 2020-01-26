245ef9221325edcea4b38f26adbe08ac58072228
bluedrink9 <bluedrink9@users.noreply.github.com>
2020-01-26 19:08:29 +1300

    Add aliases

--- shell/powershell/aliases.ps1
+++ shell/powershell/aliases.ps1
@@ -7,3 +7,6 @@ Set-Alias le liteEdit
 function ide {nvim --cmd "let g:ideMode=1"}
 Set-Alias ide ide
 Set-Alias time measure-command
+Set-Alias fopen explorer.exe
+function envupd { git -C "$DOTFILES_DIR" pull }
+# function sudo { elevate.exe -k }  # k = persistent
