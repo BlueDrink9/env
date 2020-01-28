fa26708cdd1ddf2713c8d0fd37caa93494115161
bluedrink9 <bluedrink9@users.noreply.github.com>
2020-01-28 13:23:18 +1300

    Powershell: Add git funcs/aliases

--- shell/powershell/aliases.ps1
+++ shell/powershell/aliases.ps1
@@ -1,6 +1,12 @@
 # Aliases to programs with arguments must be created as functions. Consider just using functions instead.
 Set-Alias which where.exe
 Set-Alias g git
+function gs { git status }
+function gco { git commit }
+function gca { git commit --all }
+function gup { git commit --amend --no-edit --all }
+function gupa { git commit --amend --no-edit --all }
+function gupe { git commit --amend }
 Set-Alias e nvim
 function liteEdit {nvim --cmd "let g:liteMode=1"}
 Set-Alias le liteEdit
