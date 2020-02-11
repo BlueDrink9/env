5e63481e4dbd4b5c20e376ca37329b084c8f67f9
bluedrink9 <bluedrink9@no-reply.users.github.com>
2020-02-11 20:44:13 +1300

    Windows: file associations minor refactor

--- system/Windows/fileAssociations.ps1
+++ system/Windows/fileAssociations.ps1
@@ -1,3 +1,11 @@
+
+# Won't ever work. Gets gvim.bat in sys32.
+# $editor = "$(Get-Command gvim).Path"
+
+if ([string]::IsNullOrEmpty($editor)) {
+    $editor = 'C:\tools\vim\latest\gvim.exe'
+}
+
 # Files without extensions = text
 cmd /c "assoc .=plaintext"
 
@@ -58,12 +66,8 @@ $plaintextEditMenuFTs = @(
         "VBSFile"
 )
 
-$vimbin = "$(where gvim.exe)"
-if ([string]::IsNullOrEmpty($vimbin)) {
-    $vimbin = 'C:\tools\vim\latest\gvim.exe'
-}
-cmd /c "ftype plaintext=$vimbin `"%1`""
-cmd /c "ftype text=$vimbin `"%1`""
+cmd /c "ftype plaintext=$editor `"%1`""
+cmd /c "ftype text=$editor `"%1`""
 
 # PYthon by default only has 'edit with idle' context command.
 if (-not (test-path "HKLM:\Software\Classes\Python.File\Shell\Edit\command")) {
@@ -73,5 +77,5 @@ if (-not (test-path "HKLM:\Software\Classes\Python.File\Shell\Edit\command")) {
 # Set edit menu command.
 foreach ($ft in $plaintextEditMenuFTs)
 {
-    New-ItemProperty -Path "HKLM:\Software\Classes\$ft\Shell\Edit\command" -Name "(Default)" -PropertyType String -Value "`"$vimbin`" `"%1`""
+    New-ItemProperty -Path "HKLM:\Software\Classes\$ft\Shell\Edit\command" -Name "(Default)" -PropertyType String -Value "`"$editor`" `"%1`""
 }
