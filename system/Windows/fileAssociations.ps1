448a943e5ac89aeee1dda948c87acf1cb8336161
bluedrink9 <bluedrink9@users.noreply.github.com>
2020-03-05 09:02:13 +1300

    Win: Update audio fileAssociations

--- system/Windows/fileAssociations.ps1
+++ system/Windows/fileAssociations.ps1
@@ -1,10 +1,21 @@
-
+# vim: foldmethod=marker:foldmarker={[},{]}
 # Won't ever work. Gets gvim.bat in sys32.
 # $editor = "$(Get-Command gvim).Path"
 
 if ([string]::IsNullOrEmpty($editor)) {
     $editor = 'C:\tools\vim\latest\gvim.exe'
 }
+cmd /c "ftype plaintext=$editor `"%1`""
+cmd /c "ftype text=$editor `"%1`""
+
+if ([string]::IsNullOrEmpty($mplayer)) {
+    $mplayer = 'C:\Program Files\VideoLAN\VLC\vlc.exe'
+}
+cmd /c "ftype audio=$mplayer `"%1`""
+
+########################################
+############### Text ################## {[}
+########################################
 
 # Files without extensions = text
 cmd /c "assoc .=plaintext"
@@ -67,9 +78,6 @@ $plaintextEditMenuFTs = @(
         "VBSFile"
 )
 
-cmd /c "ftype plaintext=$editor `"%1`""
-cmd /c "ftype text=$editor `"%1`""
-
 # PYthon by default only has 'edit with idle' context command.
 if (-not (test-path "HKLM:\Software\Classes\Python.File\Shell\Edit\command")) {
     New-Item -Path "HKLM:\Software\Classes\Python.File\Shell\Edit"
@@ -80,3 +88,20 @@ foreach ($ft in $plaintextEditMenuFTs)
 {
     New-ItemProperty -Path "HKLM:\Software\Classes\$ft\Shell\Edit\command" -Name "(Default)" -PropertyType String -Value "`"$editor`" `"%1`""
 }
+# {]}
+
+
+########################################
+############### Audio ################## {[}
+########################################
+$audioExtensions = @(
+    "m4a",
+    "avi",
+    "mp3"
+)
+foreach ($ext in $audioExtensions)
+{
+    cmd /c "assoc .$ext=audio"
+}
+# {]}
+
