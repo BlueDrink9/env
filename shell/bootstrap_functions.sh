428ae93b7618daf170f006e4d91b5f347881912d
BlueDrink9 <bluedrink9@github.com>
2020-03-29 10:46:58 +1300

    Further refactor of bootstrap

--- shell/bootstrap_functions.sh
+++ shell/bootstrap_functions.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# For bootstrapping (shell init) functions shared between bash, zsh, other
+# posix shells.
 
 extractPSCmdCol(){
   # The PS COMMAND col is sometimes abbreviated CMD
@@ -7,4 +9,16 @@ extractPSCmdCol(){
 getShellProgram(){
   # Get cmd of current process, last row of ps, strip leading - (osx) and trailing args.
   ps -p $$ | extractPSCmdCol | tail -1 | sed 's/^-//' | sed 's/ -.*//'
+}
+
+loadProfile(){
+  # When changing shells, force setup again.
+  SHELL_PROGRAM="$(getShellProgram)"
+  if [ "$SHELL_PROGRAM" != "$(basename "$SHELL")" ]; then
+    unset PROFILE_LOADED
+    export SHELL="$(which $SHELL_PROGRAM)"
+  fi
+  if [ -z "$PROFILE_LOADED" ]; then
+    source "${SCRIPT_DIR}"/profile.*
+  fi
 }
--- shell/variables.sh
+++ shell/variables.sh
@@ -6,5 +6,3 @@ if substrInStr "xterm" "$TERM" || substrInStr "tmux" "$TERM" || [ "$TERM_PROGRAM
   term_bar_cursor="\e[6 q"
   term_block_cursor="\e[1 q"
 fi
-
-shell="$(ps -p $$ | tail -1 | awk '{print $NF}')"
