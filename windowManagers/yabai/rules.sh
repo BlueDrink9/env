1e23b4fe6f6fed7d7f3cf785f5a04af9258c1f11
BlueDrink9 <bluedrink9@github.com>
2020-04-01 12:58:46 +1300

    Add yabai rules

---
+++ windowManagers/yabai/rules.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# https://github.com/koekeishiya/yabai/blob/master/doc/yabai.asciidoc#66-rule
+
+yabai -m rule --add app="workrave" manage=off
+yabai -m rule --add app="System Preferences" manage=on
+yabai -m rule --add label="Finder" app="^Finder$" title="(Co(py|nnect)|Move|Info|Pref)" manage=off
+yabai -m rule --add label="Calculator" app="^Calculator$" manage=off
+yabai -m rule --add label="Dictionary" app="^Dictionary$" manage=off
+yabai -m rule --add app="Archive Utility" manage=off
+yabai -m rule --add app="App Store" manage=off
+yabai -m rule --add app="Word*" manage=on
+yabai -m rule --add app="OneNote*" manage=on
+yabai -m rule --add app="PowerPoint*" manage=on
+yabai -m rule --add app="Excel*" manage=on
+yabai -m rule --add app="PowerPoint" manage=on
+# yabai -m rule --add app="PowerPoint" --subrole AXUnknown manage=off
+# yabai -m rule --add app="PowerPoint" --subrole AXDialog manage=off
+# yabai -m rule --add --subrole AXUnknown manage=off
+# yabai -m rule --add --subrole AXDialog manage=off
+yabai -m rule --add app="Excel" manage=on
+yabai -m rule --add app="Word" manage=on
+yabai -m rule --add app="OneNote" manage=on
+yabai -m rule --add app="kitty" opacity=0.9
+# XQuartz basically cannot be accessed by accessability API.
+# yabai -m rule --add app=XQuartz manage=on
+# yabai -m rule --add app=Inkscape manage=on
+
