321e8935aa38740143c5f5f7af64f83889dc2d95
bluedrink9 <bluedrink9@github.com>
2020-04-01 17:34:56 +1300

    bspwm colour fix

--- windowManagers/variables.sh
+++ windowManagers/variables.sh
@@ -1,7 +1,7 @@
 # Shared variables for window manager use.
-export preselect_border_color="d75f5f"
-export focused_border_color="eb7423"
-export unfocused_border_color="1cb5db"
-export manage_border_color="00bc00"
-export prefix_border_color="FAC863"
+export preselect_border_color="d75f5f" # pink-ish
+export focused_border_color="eb7423"  # Orange
+export unfocused_border_color="1cb5db"  # cyan
+export manage_border_color="00bc00"  # green
+export prefix_border_color="FAC863"  # mustard
 export window_resize_delta="10"
