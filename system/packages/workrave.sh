f557d62ba4c58c2ea94247f1348387b64d6ad6d2
BlueDrink9 <bluedrink9@github.com>
2020-05-07 12:24:48 +1200

    Update script for installing workrave on osx

--- system/packages/workrave.sh
+++ system/packages/workrave.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 pushd $HOME
+set -e
 mkdir -p ~/Applications ~/.local/packages ~/.local/src
+SRCDIR="$HOME/.local/src/workrave"
 # rm -rf ~/Library/Preferences/com.workrave.plist
 # rm -rf ~/Library/Preferences/workrave.plist
 # Officially required packages:
-wr_dependencies_official="gettext intltool gobject-introspection autoconf-archive gtk+ gtk-mac-integration gtkmm3"
+wr_dependencies_official="gettext intltool gobject-introspection autoconf-archive gtk+ gtk-mac-integration gtkmm3 ice"
 # These packages get it working without crashes, with caveat of no symbols/icons. IDK which one does the trick.
 wr_dependencies_extra="automake autoconf libtool gnome-icon-theme hicolor-icon-theme"
 brew install $wr_dependencies_official $wr_dependencies_extra
 brew link --force gettext libffi
-git clone --depth 1 https://github.com/palfrey/workrave/ "$HOME/.local/src/workrave"
-cd .local/src/workrave
-git checkout os-x-fixes
+rm -rf "$SRCDIR"
+git clone --depth 1 --single-branch --branch os-x-fixes https://github.com/palfrey/workrave/ "$SRCDIR"
+cd "$SRCDIR"
 autoreconf -if
 intltoolize --automake --copy --force
 ./configure --prefix="$HOME/.local/packages/workrave" --config-cache
