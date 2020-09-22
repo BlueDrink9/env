31b219292204ec4d8028612720d24f4e482ebb25
BlueDrink9 <bluedrink9@github.com>
2020-09-22 13:56:23 +1200

    Yabai: Massively increase window_resize_delta

--- windowManagers/variables.sh
+++ windowManagers/variables.sh
@@ -4,4 +4,4 @@ export focused_border_color="eb7423"  # Orange
 export unfocused_border_color="1cb5db"  # cyan
 export manage_border_color="00bc00"  # green
 export prefix_border_color="FAC863"  # mustard
-export window_resize_delta="10"
+export window_resize_delta="100"
