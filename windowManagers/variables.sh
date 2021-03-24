d51e9ba6b170380df3f0702b3af50fb2b4a078c8
bluedrink9 <bluedrink9@users.noreply.github.com>
2021-03-25 12:02:06 +1300

    Bspwm: Extract some border colours from Xresources

--- windowManagers/variables.sh
+++ windowManagers/variables.sh
@@ -5,3 +5,10 @@ export unfocused_border_color="1cb5db"  # cyan
 export manage_border_color="00bc00"  # green
 export prefix_border_color="FAC863"  # mustard
 export window_resize_delta="100"
+
+# Red
+if [ -f "$HOME/.Xresources" ]; then
+    export focused_border_color=$(xrdb ~/.Xresources -query all | grep color1 | cut -f2)
+    # Cyan
+    export unfocused_border_color=$(xrdb ~/.Xresources -query all | grep color6 | cut -f2)
+fi
