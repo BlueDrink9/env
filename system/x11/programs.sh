0256ab74db0c20b384b0186ffa6bf8867eeaa31a
bluedrink9 <bluedrink9@users.noreply.github.com>
2021-03-26 12:42:10 +1300

    change program order

--- system/x11/programs.sh
+++ system/x11/programs.sh
@@ -10,22 +10,25 @@ runWithLogging(){
   unset prog
 }
 
+# If running bspwm on its own, start other desktop elements.
+if substrInStr "bspwm" "$DESKTOP_SESSION" || \
+  [ -z "$DESKTOP_SESSION" -a "$DEFAULT_SESSION" = "bspwm" ] ; then
+  # Desktop elements
+  runWithLogging albert
+  runWithLogging polybar -c ~/.config/polybar/config.ini mybar
+  # Read by bspwm config and tdrop, although bspwm should automatically adjust
+  # for panels so may need to remove it from that.
+  export PANEL_HEIGHT=20
+  runWithLogging dunst
+  # latte-dock &
+fi
+
+# Run these after the above so that we have a taskbar for syncthing-gtk to sit in.
 # Arbitrarily decide on Wellington
 runWithLogging redshift-gtk -l -41.28664:174.77557 -t 6500:3000 -b 1:0.7
 runWithLogging workrave
 runWithLogging syncthing-gtk --minimized
 
-# If running bspwm on its own, start other desktop elements.
-if substrInStr "bspwm" "$DESKTOP_SESSION" || \
-  [ -z "$DESKTOP_SESSION" -a "$DEFAULT_SESSION" = bspwm ] ; then
-  # Desktop elements
-  runWithLogging albert
-  runWithLogging polybar -c ~/.config/polybar/config.ini mybar
-  # Read by bspwm config
-  export PANEL_HEIGHT=20
-  runWithLogging dunst
-  # latte-dock &
-fi
 
 # If started by startx, does not get set.
 if [ -z "$DESKTOP_SESSION" ]; then
