71b4f32f7fdb94c5235657c60aad5fdac31d7e43
bluedrink9 <bluedrink9@users.noreply.github.com>
2021-03-25 13:10:35 +1300

    Disable border based on xresources
    Querying does't seem to work on even the raw colourscheme file.

--- windowManagers/variables.sh
+++ windowManagers/variables.sh
@@ -6,9 +6,9 @@ export manage_border_color="00bc00"  # green
 export prefix_border_color="FAC863"  # mustard
 export window_resize_delta="100"
 
-if [ -f "$HOME/.Xresources" ]; then
-    # Red
-    export focused_border_color=$(xrdb ~/.Xresources -query all | grep color9 | cut -f2)
-    # Cyan
-    export unfocused_border_color=$(xrdb ~/.Xresources -query all | grep color14 | cut -f2)
-fi
+# if [ -f "$HOME/.Xresources" ]; then
+#     # Red
+#     export focused_border_color=$(xrdb ~/.Xresources -query all | grep color9 | cut -f2)
+#     # Cyan
+#     export unfocused_border_color=$(xrdb ~/.Xresources -query all | grep color14 | cut -f2)
+# fi
