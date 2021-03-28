842456f438c66486abec4e793344a7e3dae371c2
bluedrink9 <bluedrink9@github.com>
2021-03-29 12:07:58 +1300

    Change window border colours

--- windowManagers/variables.sh
+++ windowManagers/variables.sh
@@ -6,6 +6,10 @@ export manage_border_color="00bc00"  # green
 export prefix_border_color="FAC863"  # mustard
 export window_resize_delta="100"
 
+# Inspired by https://www.reddit.com/r/unixporn/comments/gfhons/bspwm_im_a_thief/
+export focused_border_color="13CA91"  # Teal
+export unfocused_border_color="F808E9"  # purple
+
 # if [ -f "$HOME/.Xresources" ]; then
 #     # Red
 #     export focused_border_color=$(xrdb ~/.Xresources -query all | grep color9 | cut -f2)
