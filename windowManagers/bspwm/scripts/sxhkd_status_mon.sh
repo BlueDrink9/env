45f5a978a870424c39553308d74d196562813e1d
bluedrink9 <bluedrink9@users.noreply.github.com>
2021-04-23 10:32:57 +1200

    Comment

--- windowManagers/bspwm/scripts/sxhkd_status_mon.sh
+++ windowManagers/bspwm/scripts/sxhkd_status_mon.sh
@@ -8,6 +8,7 @@ status_fifo=$1
 { while read -r line ; do 
   msg=${line#?}
   prefix=${line:0:1}
+  # See https://github.com/baskerville/sxhkd/blob/fe241b0d2d70c9c483b23cf3cd14f1383f0953a2/src/sxhkd.h#L38-L42 for all prefixes.
   case $prefix in
     B) # Begin chain
       bspc config focused_border_color "#$manage_border_color"
