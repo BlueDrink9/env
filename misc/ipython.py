d27aeb18e717532ab2e532a551a7f8c380802389
bluedrink9 <bluedrink9@users.noreply.github.com>
2021-06-17 14:36:06 +1200

    Ipython: change cursor with vi mode

--- misc/ipython.py
+++ misc/ipython.py
@@ -21,8 +21,6 @@ c.InteractiveShellApp.exec_files = [
 #  Default: 'multicolumn'
 # c.TerminalInteractiveShell.display_completions = 'multicolumn'
 
-## Shortcut style to use at the prompt. 'vi' or 'emacs'.
-c.TerminalInteractiveShell.editing_mode = 'vi'
 c.TerminalInteractiveShell.editor = 'myVim'
 ## Display the current vi mode (when using vi editing mode).
 c.TerminalInteractiveShell.prompt_includes_vi_mode = True
@@ -30,3 +28,29 @@ c.TerminalInteractiveShell.prompt_includes_vi_mode = True
 ## Use 24bit colors instead of 256 colors in prompt highlighting.
 if os.getenv("COLORTERM"):
     c.TerminalInteractiveShell.true_color = True
+
+
+import sys
+from operator import attrgetter
+from prompt_toolkit.key_binding.vi_state import InputMode, ViState
+import prompt_toolkit.key_binding.defaults as pt_defaults
+from prompt_toolkit.filters.cli import ViInsertMode
+from prompt_toolkit.keys import Keys
+
+# Change cursor for different vi modes.
+def set_input_mode(self, mode):
+    shape = {InputMode.NAVIGATION: 1, InputMode.REPLACE: 3}.get(mode, 5)
+    raw = u'\x1b[{} q'.format(shape)
+    if hasattr(sys.stdout, '_cli'):
+        out = sys.stdout._cli.output.write_raw
+    else:
+        out = sys.stdout.write
+    out(raw)
+    sys.stdout.flush()
+    self._input_mode = mode
+
+ViState._input_mode = InputMode.INSERT
+ViState.input_mode = property(attrgetter('_input_mode'), set_input_mode)
+
+
+c.TerminalInteractiveShell.editing_mode = 'vi'
