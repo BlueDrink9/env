647c623e3a192da53a7d9e4effce01e04f09ac25
bluedrink9 <bluedrink9@users.noreply.github.com>
2021-07-22 22:20:13 +1200

    Add replacement operator

--- editors/emacs/doom.d/config.el
+++ editors/emacs/doom.d/config.el
@@ -178,3 +178,13 @@
   ;; If creating commit with nothing staged, auto-stage current file.
   (setq magit-commit-ask-to-stage nil)
   )
+
+(evil-define-operator my/evil-replace-with-kill-ring (beg end)
+  "Replace with killring action."
+  :move-point nil (interactive "<r>")
+  (save-excursion (delete-region beg end)
+                  (goto-char beg)
+                  (call-interactively 'evil-paste-before 1)))
+(map! :n "d" (general-key-dispatch 'evil-delete
+                    "r" 'my/evil-replace-with-kill-ring
+                    "d" 'evil-delete-whole-line))
