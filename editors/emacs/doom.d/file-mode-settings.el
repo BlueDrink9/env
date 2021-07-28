d09fa8a46ab27c238da31d06938a4fef37d1f401
bluedrink9 <bluedrink9@users.noreply.github.com>
2021-07-28 16:45:53 +1200

    Exclude some things (including line numbers) from variable pitch

--- editors/emacs/doom.d/file-mode-settings.el
+++ editors/emacs/doom.d/file-mode-settings.el
@@ -6,6 +6,14 @@
             (variable-pitch-mode 1)
             (visual-line-mode)
             ))
+(add-hook 'text-mode-hook
+          '(lambda ()
+             (mapc
+              (lambda (face)
+                (set-face-attribute face nil :inherit 'fixed-pitch))
+              (list 'line-number
+                    'tex-verbatim
+                    ))))
 
 ;; Use soft wraps instead of hard. Only enable if found to be neccessary.
 ;; (remove-hook 'text-mode-hook #'auto-fill-mode)
@@ -27,7 +35,23 @@
 (setq markdown-fontify-code-blocks-natively 1)
 (setq markdown-header-scaling 1)
 
+;;; LaTeX
+(add-hook 'text-mode-hook
+          '(lambda ()
+             (mapc
+              (lambda (face)
+                (set-face-attribute face nil :inherit 'fixed-pitch))
+              (list 'font-latex-verbatim-face
+                    'tex-verbatim
+                    'font-latex-doctex-documentation-face
+                    'font-latex-doctex-preprocessor-face
+                    'TeX-error-description-help
+                    'TeX-error-description-warning
+                    'TeX-error-description-tex-said
+                    ))))
+
 ;;; Org
+;; Exclude these faces from using variable-pitch fonts.
 (add-hook 'org-mode-hook
             '(lambda ()
                (mapc
