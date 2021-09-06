9b370f2bbce34809a22437b48dd74acc70a3c25d
bluedrink9 <bluedrink9@github.com>
2021-09-06 20:05:46 +1200

    Doom: Change python lsp to pyright

--- editors/emacs/doom.d/init.el
+++ editors/emacs/doom.d/init.el
@@ -177,7 +177,12 @@
        php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +lsp)            ; beautiful is better than ugly
+       (python             ; beautiful is better than ugly
+        +lsp
+        +pyright          ; Use the pyright LSP server instead of mspyls or pyls
+        +pyenv            ; Python virtual environment support via pyenv
+        +conda            ; Python virtual environment support via Conda
+        )
        qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
