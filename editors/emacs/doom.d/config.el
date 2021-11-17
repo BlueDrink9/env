070cceff0d299ccc2b6ed2f4c9317902b45c8257
bluedrink9 <bluedrink9@github.com>
2021-11-17 22:41:05 +1300

    doom: set org dir ~/org

--- editors/emacs/doom.d/config.el
+++ editors/emacs/doom.d/config.el
@@ -41,7 +41,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/work/org/")
+(setq org-directory "~/org")
 
 (setq abbrev-file-name             ;; tell emacs where to read abbrev
       (concat script_dir "../abbrev_defs"))
