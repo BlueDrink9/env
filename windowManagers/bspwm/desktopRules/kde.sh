5aacf9d174b5e0fa8739df40c18d7be34a7c2f10
BlueDrink9 <BlueDrink9>
2022-06-27 13:20:09 +1200

    bspwm: Try to automatically get plasma panel height

--- windowManagers/bspwm/desktopRules/kde.sh
+++ windowManagers/bspwm/desktopRules/kde.sh
@@ -1,5 +1,19 @@
 #! /bin/sh
 
+get_plasma_panel_height(){
+    fallback=35
+    config="$HOME/.config/plasmashellrc"
+    if [ -f "$config" ]; then
+        # Probably won't work for multiple panels, but it'll do for now
+        line=$(grep thickness "$config")
+        height=$(echo "$line" | cut -d "=" -f 2 | head -n1)
+        echo $height
+    else
+        echo $fallback
+    fi
+}
+export PANEL_HEIGHT=$(get_plasma_panel_height)
+
 bspc rule -a plasmashell    border=off, state=floating, center=off
 bspc rule -a plasma-desktop state=floating, border=off
 bspc rule -a Plasma         state=floating, border=off
@@ -15,5 +29,5 @@ bspc rule -a mplayer2       state=floating, border=off
 # bspc config left_padding 0
 # bspc config top_padding 0
 # Enough to avoid taskbar
-bspc config bottom_padding 25
+bspc config bottom_padding $PANEL_HEIGHT
 
