b26e8295944186e2f457191b442d40a8581c2e68
bluedrink9 <bluedrink9@users.noreply.github.com>
2022-08-29 09:13:33 +1200

    Reindent lua to 3 spaces
    as per guide

--- editors/vim/plugins/nvim-lspconfig.lua
+++ editors/vim/plugins/nvim-lspconfig.lua
@@ -1,77 +1,77 @@
 if vim.g.plugs["nvim_lsp"] == nil then
-  return
+   return
 end
 nvim_lsp = require('lspconfig')
 nvim_lspconfig_server_setup(nvim_lsp)
 -- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
 require'lspinstall'.post_install_hook = function ()
-  nvim_lspconfig_server_setup() -- reload installed servers
-  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+   nvim_lspconfig_server_setup() -- reload installed servers
+   vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
 end
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-  --Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+   --Enable completion triggered by <c-x><c-o>
+   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  local opts = { noremap=true, silent=true }
-  local maps = vim.api.nvim_get_var('IDE_mappings')
+   -- Mappings.
+   local opts = { noremap=true, silent=true }
+   local maps = vim.api.nvim_get_var('IDE_mappings')
 
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', maps.implementation, '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', maps.implementation2, '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', maps.definition, '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', maps.documentation, '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', maps.documentation2, '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', maps.documentation3, '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', maps.implementation, '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', maps.implementation2, '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', maps.signature, '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  -- buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  -- buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  -- buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', maps.type_definition, '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', maps.type_definition2, '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', maps.rename, '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', maps.codeAction, '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', maps.references, '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', map.diagnostic, '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', maps.diagnostic_next, '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', maps.diagnostic_prev, '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  -- buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  buf_set_keymap("n", maps.reformat, "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+   -- See `:help vim.lsp.*` for documentation on any of the below functions
+   buf_set_keymap('n', maps.implementation, '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+   buf_set_keymap('n', maps.implementation2, '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+   buf_set_keymap('n', maps.definition, '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+   buf_set_keymap('n', maps.documentation, '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+   buf_set_keymap('n', maps.documentation2, '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+   buf_set_keymap('n', maps.documentation3, '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+   buf_set_keymap('n', maps.implementation, '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+   buf_set_keymap('n', maps.implementation2, '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+   buf_set_keymap('n', maps.signature, '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+   -- buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+   -- buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+   -- buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+   buf_set_keymap('n', maps.type_definition, '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+   buf_set_keymap('n', maps.type_definition2, '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+   buf_set_keymap('n', maps.rename, '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+   buf_set_keymap('n', maps.codeAction, '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+   buf_set_keymap('n', maps.references, '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+   buf_set_keymap('n', map.diagnostic, '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+   buf_set_keymap('n', maps.diagnostic_next, '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+   buf_set_keymap('n', maps.diagnostic_prev, '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+   -- buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+   buf_set_keymap("n", maps.reformat, "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 
 end
 
 nvim_lspconfig_server_setup = function(nvim_lsp)
-  local lsp_installer = require("nvim-lsp-installer")
-  lsp_installer.settings({
-    ui = {
-      icons = {
-        server_installed = "✓",
-        server_pending = "➜",
-        server_uninstalled = "✗"
-      }
-    }
-  })
-  -- Register a handler that will be called for each installed server when it's ready (i.e. when installation is finished
-  -- or if the server is already installed).
-  lsp_installer.on_server_ready(function(server)
-    local opts = {}
+   local lsp_installer = require("nvim-lsp-installer")
+   lsp_installer.settings({
+         ui = {
+            icons = {
+               server_installed = "✓",
+               server_pending = "➜",
+               server_uninstalled = "✗"
+            }
+         }
+      })
+   -- Register a handler that will be called for each installed server when it's ready (i.e. when installation is finished
+   -- or if the server is already installed).
+   lsp_installer.on_server_ready(function(server)
+      local opts = {}
 
-    -- (optional) Customize the options passed to the server
-    -- if server.name == "tsserver" then
-    --     opts.root_dir = function() ... end
-    -- end
+      -- (optional) Customize the options passed to the server
+      -- if server.name == "tsserver" then
+      --     opts.root_dir = function() ... end
+      -- end
 
-    -- This setup() function will take the provided server configuration and decorate it with the necessary properties
-    -- before passing it onwards to lspconfig.
-    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-    server:setup(opts)
-  end)
+      -- This setup() function will take the provided server configuration and decorate it with the necessary properties
+      -- before passing it onwards to lspconfig.
+      -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+      server:setup(opts)
+   end)
 end
--- editors/vim/plugins/treesitter.lua
+++ editors/vim/plugins/treesitter.lua
@@ -2,12 +2,12 @@ require('refactoring').setup({})
 local maps = vim.api.nvim_get_var('IDE_mappings')
 -- prompt for a refactor to apply when the remap is triggered
 for _, mode in pairs({'v', 'n'}) do
-    vim.api.nvim_set_keymap(
-    mode,
-    maps.refactor,
-    ":lua require('refactoring').select_refactor()<CR>",
-    { noremap = true, silent = true, expr = false }
-    )
+   vim.api.nvim_set_keymap(
+      mode,
+      maps.refactor,
+      ":lua require('refactoring').select_refactor()<CR>",
+      { noremap = true, silent = true, expr = false }
+      )
 end
 
 -- generate annotations (eg docstrings)
@@ -15,17 +15,17 @@ require('neogen').setup {}
 vim.cmd("command! Annotate lua require('neogen').generate()")
 
 require('nvim-treesitter.configs').setup {
-    -- ensure_installed = {
-    --     "lua",
-    --     "python",
-    -- },
-    endwise = { enable = true, },
-    highlight = {
+   -- ensure_installed = {
+   --     "lua",
+   --     "python",
+   -- },
+   endwise = { enable = true, },
+   highlight = {
       enable = true,
       -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
       -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
       -- Using this option may slow down your editor, and you may see some duplicate highlights.
       -- Instead of true it can also be a list of languages
       -- additional_vim_regex_highlighting = false,
-    },
+   },
 }
