246279b054bd0456c0ee2d1a3ea85cd8b936ca55
bluedrink9 <bluedrink9@users.noreply.github.com>
2022-12-02 13:42:06 +1300

    Failed attempt to highlight lualine tabline modified buffer

--- editors/vim/plugins/lualine.lua
+++ editors/vim/plugins/lualine.lua
@@ -1,3 +1,37 @@
+function get_color(group, attr)
+   local fn = vim.fn
+   return fn.synIDattr(fn.synIDtrans(fn.hlID(group)), attr)
+end
+
+local coloured_on_modified_buffers = require('lualine.components.buffers'):extend()
+local highlight = require'lualine.highlight'
+function coloured_on_modified_buffers:init(options)
+   coloured_on_modified_buffers.super.init(self, options)
+   -- self.status_colors = {
+   --    saved = highlight.create_component_highlight_group(
+   --       {bg = get_color('lualine_a_normal', 'bg')},
+   --       'filename_status_saved', self.options),
+   --    modified = highlight.create_component_highlight_group(
+   --       {bg = get_color('GitGutterChange', 'fg')},
+   --       'filename_status_modified', self.options),
+   -- }
+   -- if self.options.color == nil then self.options.color = '' end
+end
+
+function coloured_on_modified_buffers:render()
+   local line = coloured_on_modified_buffers.super.render(self)
+   print(self.current)
+   print('arst ')
+   -- line = highlight.component_format_highlight(
+   --     self.highlights[(self.current and 'active' or 'inactive')]
+   --  ) .. line
+
+  -- line = highlight.component_format_highlight(vim.bo.modified
+  --                                             and self.status_colors.modified
+  --                                             or self.status_colors.saved) .. line
+  return line
+end
+
 local location_format = ''
 local config = {
    options = {
@@ -21,7 +55,9 @@ local config = {
    sections = {
       lualine_a = {'mode'},
       lualine_b = {'branch', 'diff', 'diagnostics'},
-      lualine_c = {{'filename', path = 1, shorting_target = vim.o.columns / 2.5}},
+      lualine_c = {
+         {'filename', path = 1, shorting_target = vim.o.columns / 2.5}
+      },
       lualine_x = {'encoding', 'fileformat', 'filetype'},
       lualine_y = {'progress'},
       lualine_z = {'searchcount', 'location'}
@@ -37,7 +73,8 @@ local config = {
    tabline = {
       lualine_a = {
          {
-            'buffers',
+            -- 'buffers',
+            coloured_on_modified_buffers,
             show_filename_only = false, -- show shortened relative path
             mode = 4, -- Show buf name+number
             -- max_length = vim.o.columns * 4 / 5,
@@ -70,9 +107,6 @@ for plugin, extension in pairs(used_extensions) do
    end
 end
 
-require('lualine').setup(config)
-
-
 vim.api.nvim_create_user_command('LualineHide', require('lualine').hide,
    { force = true, desc = 'Hide lualine'})
 
@@ -80,3 +114,4 @@ vim.api.nvim_create_user_command('LualineShow',
    function() require('lualine').hide({unhide=true}) end,
    { force = true, desc = 'Unhide lualine'})
 
+require('lualine').setup(config)
