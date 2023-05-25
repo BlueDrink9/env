27ec91f3a9677a8b0f6ec5ce2b23b418eda17e9a
BlueDrink9 <BlueDrink9>
2023-05-26 10:58:59 +1200

    add git vim to path as a nice wee backup

--- shell/powershell/settings.ps1
+++ shell/powershell/settings.ps1
@@ -1,6 +1,11 @@
 $env:USEPF = 1
 $env:TERMCOLOR = 16
 
+$git_usr_bin=$(Join-path `
+        -path $(split-path $(get-command git.exe).Path -parent)`
+        -childpath "..\usr\bin")
+$Env:Path += [IO.Path]::PathSeparator + "$git_usr_bin"
+
 $Shell = $Host.UI.RawUI
 # $Shell.WindowTitle="SysadminGeek"
 # $size = $Shell.WindowSize
