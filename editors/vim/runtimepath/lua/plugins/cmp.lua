ad808292a838181f184f154cf275b53ca779cc1d
bluedrink9 <bluedrink9@users.noreply.github.com>
2023-08-24 10:03:35 +1200

    Fix cmp load

--- editors/vim/runtimepath/lua/plugins/cmp.lua
+++ editors/vim/runtimepath/lua/plugins/cmp.lua
@@ -2,53 +2,35 @@ if vim.g.ideMode==0 then
   return {}
 end
 
-local maps = vim.api.nvim_get_var('IDE_mappings')
-
--- https://github.com/ray-x/lsp_signature.nvim
-vim.o.completeopt = "menuone,noselect"
-local cmp_buffer = require('cmp_buffer')
-
-Get_bufnrs_to_complete_from = function()
-  -- Include all visible buffers (not just default of current one) below a certain size.
-  local bufs = {}
-  for _, win in ipairs(vim.api.nvim_list_wins()) do
-    local buf = vim.api.nvim_win_get_buf(win)
-    local byte_size = vim.api.nvim_buf_get_offset(
-      buf, vim.api.nvim_buf_line_count(buf))
-    if byte_size < 1024 * 1024 then -- 1 Megabyte max
-      bufs[buf] = true
-    end
-  end
-  return vim.tbl_keys(bufs)
-end
-
 return {
+
   {'hrsh7th/nvim-cmp',
-    opts={
-      -- debug = false;
-      -- min_length = 1;
-      preselect = require'cmp'.PreselectMode.None;
-      -- view = {
-      --   entries = {name = 'custom', selection_order = 'near_cursor' }
-      -- },
-      performance = {
-        debounce = 200,
-        throttle = 1,
-      },
+    opts = function(_, opts)
+      local cmp = require("cmp")
 
-      snippet = {
-        expand = function(args)
-          vim.fn["vsnip#anonymous"](args.body)
-          -- require('luasnip').lsp_expand(args.body)
-        end,
-      },
+      Get_bufnrs_to_complete_from = function()
+        -- Include all visible buffers (not just default of current one) below a certain size.
+        local bufs = {}
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          local byte_size = vim.api.nvim_buf_get_offset(
+            buf, vim.api.nvim_buf_line_count(buf))
+          if byte_size < 1024 * 1024 then -- 1 Megabyte max
+            bufs[buf] = true
+          end
+        end
+        return vim.tbl_keys(bufs)
+      end
 
-      sources = require'cmp'.config.sources({
-        { name = "nvim_lsp",
-          entry_filter = function(entry, ctx)
-            -- Dont suggest Text from nvm_lsp
-            return require('require'cmp'.types').lsp.CompletionItemKind[entry:get_kind()] ~= 'Text'
-          end },
+
+      opts.preselect = cmp.PreselectMode.None;
+
+      local sources = {
+        -- { name = "nvim_lsp",
+        --   entry_filter = function(entry, ctx)
+        --     -- Dont suggest Text from nvm_lsp
+        --     return require('cmp.types').lsp.CompletionItemKind[entry:get_kind()] ~= 'Text'
+        --   end },
         {
           name = 'buffer',
           option = {
@@ -57,14 +39,16 @@ return {
           },
           sorting = {
             comparators = {
-              function(...) return cmp_buffer:compare_locality(...) end,
+              function(...) return require'cmp-buffer':compare_locality(...) end,
             },
           }
         },
-        { name = 'vsnip' },
-        { name = 'path' },
-        { name = 'nvim_lsp_signature_help' },
-        { name = 'vim-dadbod-completion' },
+
+        -- { name = 'vsnip' },
+        -- { name = 'path' },
+        -- { name = 'nvim_lsp_signature_help' },
+        -- { name = 'vim-dadbod-completion' },
+
         {
           name = 'spell',
           option = {
@@ -75,73 +59,80 @@ return {
             end,
           },
         },
-        sorting = {
-          comparators = {
-            function(...) return cmp_buffer:compare_locality(...) end,
-          }
-        }
-      }),
 
-      mapping = {
-        ['<C-e>'] = require'cmp'.mapping.confirm({ select = false }),
+        -- sorting = {
+        --   comparators = {
+        --     function(...) return cmp_buffer:compare_locality(...) end,
+        --   }
+        -- }
+      }
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources or {}, sources))
+
+
+      opts.mapping = {
+        ['<C-e>'] = cmp.mapping.confirm({ select = false }),
         -- Because I tab and go, the current selection when I push space is already the one I want.
         -- But this _is_ useful for snippets, because they will be expanded, not just selected.
         ['<space>'] = function(fallback)
-          if require'cmp'.visible() and require'cmp'.expandable then
-            require'cmp'.mapping.confirm({ select = false })
+          if cmp.visible() and cmp.expandable then
+            cmp.mapping.confirm({ select = false })
             vim.fn.feedkeys(' ')
           else
             fallback()
           end
         end,
-        ["<Tab>"] = require'cmp'.mapping(function(fallback)
-          if require'cmp'.visible() then
-            require'cmp'.select_next_item()
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
           else
             fallback()
           end
         end, {'i', 'c'}),
-        ["<S-Tab>"] = require'cmp'.mapping(function(fallback)
-          if require'cmp'.visible() then
-            require'cmp'.select_prev_item()
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
           else
             fallback()
           end
         end, {'i', 'c'}),
-        ['<C-b>'] = require'cmp'.mapping(require'cmp'.mapping.scroll_docs(-4), { 'i' }),
-        ['<C-f>'] = require'cmp'.mapping(require'cmp'.mapping.scroll_docs(4), { 'i' }),
-      },
-
-    },
+        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i' }),
+        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i' }),
+      }
+    end,
 
     config=function ()
 
-      require'cmp'.setup.cmdline(':', {
-        sources = require'cmp'.config.sources({
-          { name = 'path' },
-          { name = 'cmdline' }
-        })
-      })
+      -- cmp.setup.cmdline(':', {
+      --   sources = cmp.config.sources({
+      --     { name = 'path' },
+      --     { name = 'cmdline' }
+      --   })
+      -- })
 
-      require'cmp'.setup.cmdline('/', {
-        sources = require'cmp'.config.sources({
-          { name = 'buffer' },
-          { name = 'nvim_lsp_document_symbol' }
-        })
-      })
+      -- cmp.setup.cmdline('/', {
+      --   sources = cmp.config.sources({
+      --     { name = 'buffer' },
+      --     { name = 'nvim_lsp_document_symbol' }
+      --   })
+      -- })
 
-      -- Load vsnip when file opens, to avoid lag on insert mode.
-      -- Will see whether this slows down opening a file though...
-      vim.api.nvim_exec([[
-autocmd InsertEnter * call vsnip#get_complete_items(bufnr())
-]], false)
+--       -- Load vsnip when file opens, to avoid lag on insert mode.
+--       -- Will see whether this slows down opening a file though...
+--       vim.api.nvim_exec([[
+-- autocmd InsertEnter * call vsnip#get_complete_items(bufnr())
+-- ]], false)
+
+    end,
+    init = function()
+      -- -- https://github.com/ray-x/lsp_signature.nvim
+      -- vim.o.completeopt = "menuone,noselect"
 
       -- Debounce tabstop syncing.
-      vim.g.vsnip_sync_delay = 20
-      vim.g.vsnip_choice_delay = 250
+      -- vim.g.vsnip_sync_delay = 20
+      -- vim.g.vsnip_choice_delay = 250
+    end,
 
-    end
-  }
+  },
 
   -- -- Tab and S-Tab keys need to be mapped to <C-n> and <C-p> when completion menu is visible. Following example will use Tab and S-Tab (shift+tab) to navigate completion menu and jump between vim-vsnip placeholders when possible:
 
@@ -165,7 +156,7 @@ autocmd InsertEnter * call vsnip#get_complete_items(bufnr())
   --   elseif check_back_space() then
   --     return t "<Tab>"
   --   else
-  --     return vim.fn['require'cmp'#complete']()
+  --     return vim.fn['cmp#complete']()
   --   end
   -- end
   -- _G.s_tab_complete = function()
