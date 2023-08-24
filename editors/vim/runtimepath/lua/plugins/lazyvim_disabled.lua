b6aba926c09e29bec4d3bf11c83e484f3d2aa14c
bluedrink9 <bluedrink9@users.noreply.github.com>
2023-08-24 16:52:21 +1200

    disables

--- editors/vim/runtimepath/lua/plugins/lazyvim_disabled.lua
+++ editors/vim/runtimepath/lua/plugins/lazyvim_disabled.lua
@@ -2,6 +2,8 @@ return {
   {"folke/noice.nvim", enabled = false},
   {"echasnovski/mini.pairs", enabled = false},
   {"folke/tokyonight.nvim", enabled = false},
+  {"ggandor/flit.nvim", enabled = false},
+  {"folke/persistence.nvim", enabled = false},
 
   -- {"RRethy/vim-illuminate", enabled = false},
 }
