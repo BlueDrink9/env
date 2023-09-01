97dba699aadb6d17f7c2c801f2a13ede49799ab9
BlueDrink9 <BlueDrink9>
2023-09-01 12:18:40 +1200

    Disable mini.indentscope

--- editors/vim/runtimepath/lua/plugins/lazyvim_disabled.lua
+++ editors/vim/runtimepath/lua/plugins/lazyvim_disabled.lua
@@ -2,6 +2,7 @@ return {
   {"folke/noice.nvim", enabled = false},
   {"rcarriga/nvim-notify", enabled = false},
   {"echasnovski/mini.pairs", enabled = false},
+  {"echasnovski/mini.indentscope", enabled = false},
   {"folke/tokyonight.nvim", enabled = false},
   {"ggandor/flit.nvim", enabled = false},
   {"folke/persistence.nvim", enabled = false},
