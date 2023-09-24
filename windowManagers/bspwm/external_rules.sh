1d813fd7b07097bb57307e697e294c59a5f779c1
bluedrink9 <bluedrink9@github.com>
2023-09-24 13:04:55 +1300

    Fix zooms starting window not tiling

--- windowManagers/bspwm/external_rules.sh
+++ windowManagers/bspwm/external_rules.sh
@@ -2,7 +2,7 @@
 wid=$1
 class=$2
 instance=$3
-consequences=$4
+initial_consequences=$4
 title="$(xdotool getwindowname "$wid")"
 
 case "$title" in
@@ -10,19 +10,38 @@ case "$title" in
   # should be.
   # Main meeting window
   "*Zoom Meeting")
-    eval "$consequences"
-    [ "$state" ] || echo "state=tiled"
-    ;;
+    eval "$initial_consequences"
+    echo "state=tiled"
+  ;;
   # Main application window, not in a meeting. Also meeting join windows.
   "*Zoom*")
-    eval "$consequences"
-    [ "$state" ] || echo "state=tiled"
-    ;;
+    eval "$initial_consequences"
+    echo "state=tiled"
+  ;;
   # Misc side windows etc.
   "zoom")
-    eval "$consequences"
-    [ "$state" ] || echo "state=floating center=on follow=on border=off"
-    ;;
+    eval "$initial_consequences"
+    echo "state=floating center=on follow=on border=off focus=off"
+    # zoom's main window will start titled 'zoom' initially, then change.
+    # So for 'zoom' windows, wait a second after setting state just in case
+    # the window is actually that first one.
+    # https://www.reddit.com/r/i3wm/comments/p0fdxa
+    if ! command -v xprop > /dev/null || \
+      ! command -v timeout > /dev/null; then
+        break
+    fi
+    # Monitor the Zoom window for title changes using xprop
+    timeout 0.3s xprop -id "$wid" -spy _NET_WM_NAME | while read -r line; do
+      # Remove prefix up to and including the first double quote
+      title="${line#*\"}"
+      # Remove suffix starting from the last double quote
+      title="${title%\"*}"
+      if [[ "$title" == Zoom* ]]; then
+        echo "state=tiled center=off follow=on focus=on border=off"
+        break
+      fi
+    done
+;;
 esac
 
 # if [ "$instance" = fontforge ] ; then
@@ -33,12 +52,3 @@ esac
 # 			;;
 # 	esac
 # fi
-
-# case "$class" in
-
-	# zoom)
-	# 	eval "$consequences"
-	# 	[ "$state" ] || echo "state=pseudo_tiled"
-	# 	;;
-
-# esac
