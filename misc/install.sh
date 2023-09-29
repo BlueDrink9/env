e2c3ded1df2316ea9b3a4e979e692b8d3017ef5e
BlueDrink9 <BlueDrink9>
2023-09-29 13:05:23 +1300

    vieb config

--- misc/install.sh
+++ misc/install.sh
@@ -10,6 +10,11 @@ baseRC="${HOME}/.ipython/profile_default/ipython_config.py"
 
 source "$DOTFILES_DIR/generic_rc_installer.sh"
 
+installID="Vieb"
+installText="source $($SCRIPTDIR_CMD)/viebrc"
+baseRC="${HOME}/.vieb/viebrc"
+source "$DOTFILES_DIR/generic_rc_installer.sh"
+
 installID="Pyvim"
 # installText="with open(r'$DOTFILES_DIR/editors/pyvim/pyvimrc') as infile: exec(infile.read())"
 installText="from importlib.machinery import SourceFileLoader\nconfigure = SourceFileLoader('mypyvimrc', '$DOTFILES_DIR/editors/pyvim/pyvimrc').load_module().configure"
@@ -18,6 +23,9 @@ baseRC="${HOME}/.pyvimrc"
 source "$DOTFILES_DIR/generic_rc_installer.sh"
 
 doXremap(){
+  if userHasSudo; then
+    return
+  fi
   # Best installed with cargo I think.
   # https://github.com/k0kubun/xremap
   # Ubuntu setup rules
@@ -32,5 +40,6 @@ doXremap(){
 # If directly run instead of sourced, do all
 if [ ! "${BASH_SOURCE[0]}" != "${0}" ]; then
   do${installID}
+  doVieb
   doXremap
 fi
