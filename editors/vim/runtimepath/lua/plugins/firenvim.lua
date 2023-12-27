99f7d5f12a2e5173eae02e9060c7e78c81c4e2f7
bluedrink9 <bluedrink9@github.com>
2023-12-27 17:13:24 +1300

    Firenvim: smaller autowrite timer

--- editors/vim/runtimepath/lua/plugins/firenvim.lua
+++ editors/vim/runtimepath/lua/plugins/firenvim.lua
@@ -128,12 +128,12 @@ spec.config = function()
     vim.api.nvim_create_autocmd({'TextChanged', 'TextChangedI'}, {
         group="myPlugins",
         callback = function()
-            if vim.g.timer_started == true then
+            if vim.g.autowrite_timer_started == true then
                 return
             end
-            vim.g.timer_started = true
-            vim.fn.timer_start(10000, function()
-                vim.g.timer_started = false
+            vim.g.autowrite_timer_started = true
+            vim.fn.timer_start(2000, function()
+                vim.g.autowrite_timer_started = false
                 vim.cmd('write')
             end)
         end
