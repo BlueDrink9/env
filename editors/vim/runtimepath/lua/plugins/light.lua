ada367c4c17c4bd88b331a6eaa35b895e50bc266
William.Warren <William.x.Warren@pwc.com>
2024-03-01 12:20:46 +1300

    Add custom surrounds

--- editors/vim/runtimepath/lua/plugins/light.lua
+++ editors/vim/runtimepath/lua/plugins/light.lua
@@ -34,9 +34,89 @@ return {
         insert = nil,
         insert_line = nil,
         visual = "S",
-        visual_line = "S",
+        visual_line = "gS",
       },
     },
+    config = function(_, opts)
+
+      -- custom surrounds
+      local key_index_surround = {
+        ["k"] = {
+          add = quoted_surround,
+          find = quoted_pattern,
+          delete = quoted_pattern,
+          change = {
+            target = quoted_pattern,
+          },
+        },
+        ["K"] = {
+          add = unquoted_surround,
+          find = unquoted_pattern,
+          delete = unquoted_pattern,
+          change = {
+            target = unquoted_pattern,
+          },
+        },
+      }
+
+      local surrounds = {
+        -- Keys = filetypes, then letter mapping for that surround.
+        markdown = {
+          -- create link from clipboard
+          l = {
+            add = function()
+              local clipboard = vim.fn.getreg("+"):gsub("\n", "")
+              return {
+                { "[" },
+                { "](" .. clipboard .. ")" },
+              }
+            end,
+            find = "%b[]%b()",
+            delete = "^(%[)().-(%]%b())()$",
+            change = {
+              target = "^()()%b[]%((.-)()%)$",
+              replacement = function()
+                local clipboard = vim.fn.getreg("+"):gsub("\n", "")
+                return {
+                  { "" },
+                  { clipboard },
+                }
+              end,
+            },
+          },
+        },
+        latex = {
+          -- Command and environment
+          c = {
+            add = function()
+              local cmd = require("nvim-surround.config").get_input "Command: "
+              return { { "\\" .. cmd .. "{" }, { "}" } }
+            end,
+          },
+          e = {
+            add = function()
+              local env = require("nvim-surround.config").get_input "Environment: "
+              return { { "\\begin{" .. env .. "}" }, { "\\end{" .. env .. "}" } }
+            end,
+          },
+        },
+        python = key_index_surround,
+        lua = key_index_surround,
+      }
+
+      for filetype, letters in pairs(surrounds) do
+        vim.api.nvim_create_autocmd({"FileType"}, {
+          pattern = filetype,
+          callback = function()
+            require("nvim-surround").buffer_setup({
+              surrounds = letters
+            })
+          end
+        })
+      end
+
+      require("nvim-surround").setup(opts)
+    end,
   },
 
   {"gbprod/substitute.nvim",
