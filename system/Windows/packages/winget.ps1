925314eda36477abd74636a6a5cea86211c17406
William.Warren <William.x.Warren@pwc.com>
2024-05-20 14:32:19 +1200

    Windows: Add winget package install script

---
+++ system/Windows/packages/winget.ps1
@@ -0,0 +1,27 @@
+winget install -e --id `
+BurntSushi.ripgrep.GNU `
+Neovim.Neovim `
+sharkdp.fd `
+Alacritty.Alacritty `
+Python.Python.3.11 `
+RProject.R `
+JesseDuffield.lazygit `
+GNU.Emacs `
+VSCodium.VSCodium `
+Starship.Starship `
+vim.vim `
+Flow-Launcher.Flow-Launcher `
+Joplin.Joplin `
+Microsoft.PowerShell.Preview `
+Microsoft.WindowsTerminal `
+AgileBits.1Password `
+Git.Git `
+LGUG2Z.komorebi `
+AutoHotkey.AutoHotkey `
+Espanso.Espanso `
+Jelmerro.Vieb `
+Workrave.Workrave `
+SyncTrayzor.SyncTrayzor `
+KeePassXCTeam.KeePassXC `
+Neovide.Neovide `
+JohnMacFarlane.Pandoc
