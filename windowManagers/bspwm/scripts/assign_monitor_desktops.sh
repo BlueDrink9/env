2977d76e0c6c09fc67429e87ce62188d31b426c6
bluedrink9 <bluedrink9@github.com>
2024-07-08 11:08:17 +1200

    Don't use bspc IDs

--- windowManagers/bspwm/scripts/assign_monitor_desktops.sh
+++ windowManagers/bspwm/scripts/assign_monitor_desktops.sh
@@ -7,25 +7,14 @@ set -o pipefail
 # Read the xrandr output into a variable. Cut off the first line, which
 # says how many monitors there are.
 XRANDR_OUTPUT=$(xrandr --listmonitors | tail -n +2)
-BSPC_MONITOR_NAME_ID_MAP=$(paste <(bspc query --monitors --names) <(bspc query --monitors))
-printf "BSPC_MONITOR_NAME_ID_MAP:\n%s\n" "$BSPC_MONITOR_NAME_ID_MAP"
 
 # Function to get the number of desktops for a monitor
 get_desktop_count() {
   bspc query --desktops --monitor "$1" | wc -l
 }
 
-# Get bspwm id for a monitor name
-get_monitor_id() {
-  lookup="$1"
-  echo "$BSPC_MONITOR_NAME_ID_MAP" | while IFS= read -r line; do
-    key="${line%%$'\t'*}"
-    value="${line#*$'\t'}"
-    if [ "$lookup" = "$key" ]; then
-      echo "$value"
-      return
-    fi
-  done
+bspcdo(){
+  bspc "$@"
 }
 
 # Temporary storage for other monitors
@@ -42,7 +31,6 @@ EOF
 
   if echo "$line" | grep -q "\*"; then
     PRIMARY_MONITOR=$MONITOR
-    PRIMARY_MONITOR_ID=$(get_monitor_id "$MONITOR")
     PRIMARY_RESOLUTION=$RESOLUTION
     PRIMARY_POSITION=$POSITION
     PRIMARY_POSITION_H="${POSITION%%\+*}"
@@ -53,11 +41,11 @@ EOF
 done <<< "$XRANDR_OUTPUT"
 
 # Set up the primary monitor
-if ! bspc query --monitors | grep -q "^$PRIMARY_MONITOR_ID$"; then
-  bspc wm --add-monitor "$PRIMARY_MONITOR_ID" "$PRIMARY_RESOLUTION" "$PRIMARY_POSITION"
+if ! bspc query --monitors --names | grep -q "^$PRIMARY_MONITOR$"; then
+  bspcdo wm --add-monitor "$PRIMARY_MONITOR" "$PRIMARY_RESOLUTION" "$PRIMARY_POSITION"
 fi
-if [ "$(get_desktop_count "$PRIMARY_MONITOR_ID")" -lt 4 ]; then
-  bspc monitor "$PRIMARY_MONITOR_ID" --reset-desktops " " " " " " "4"
+if [ "$(get_desktop_count %"$PRIMARY_MONITOR")" -lt 4 ]; then
+  bspcdo monitor %"$PRIMARY_MONITOR" --reset-desktops " " " " " " "4"
 fi
 
 # Initialize counters for left and right monitors
@@ -67,7 +55,6 @@ RIGHT_COUNT=0
 # Assign desktops to other monitors
 while IFS= read -r INFO; do
   MONITOR=$(echo "$INFO" | awk '{print $1}')
-  MONITOR_ID=$(get_monitor_id "$MONITOR")
   RESOLUTION=$(echo "$INFO" | awk '{print $2}')
   POSITION=$(echo "$INFO" | awk '{print $3}')
   POSITION_H="${POSITION%%\+*}"
@@ -81,10 +68,10 @@ while IFS= read -r INFO; do
     DESKTOP_NAME=$(printf '%0.s>' $(seq 1 $RIGHT_COUNT))
   fi
 
-  if ! bspc query --monitors | grep -q "^$MONITOR_ID$"; then
-    bspc wm --add-monitor "$MONITOR_ID" "$RESOLUTION" "$POSITION"
+  if ! bspc query --monitors --names | grep -q "^$MONITOR$"; then
+    bspcdo wm --add-monitor "$MONITOR" "$RESOLUTION" "$POSITION"
   fi
-  if [ "$(get_desktop_count "$MONITOR_ID")" -lt 2 ]; then
-    bspc monitor "$MONITOR_ID" --reset-desktops "$DESKTOP_NAME" "$DESKTOP_NAME"
+  if [ "$(get_desktop_count %"$MONITOR")" -lt 2 ]; then
+    bspcdo monitor %"$MONITOR" --reset-desktops "$DESKTOP_NAME" "$DESKTOP_NAME"
   fi
 done <<< "$MONITORS_INFO" 
