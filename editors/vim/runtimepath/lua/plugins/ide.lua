fdafe9d3e6607bd30814725a963e6f31301858a7
bluedrink9 <bluedrink9@github.com>
2024-07-17 16:25:59 +1200

    Add venv-selector

--- editors/vim/runtimepath/lua/plugins/ide.lua
+++ editors/vim/runtimepath/lua/plugins/ide.lua
@@ -428,5 +428,17 @@ return {
 				}
 			}
 		},
-	}
+	},
+
+	{
+		'linux-cultist/venv-selector.nvim',
+		dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim' },
+		opts = {
+			name = {"venv", ".venv"},
+			auto_refresh = true,
+			dap_enabled = true,
+		},
+		cmd = { "VenvSelect", "VenvSelectCached" },
+	},
+
 }
