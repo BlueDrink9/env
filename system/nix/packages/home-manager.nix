4345cfc5fedd8d92108a1c212900911317c4f162
bluedrink9 <bluedrink9@github.com>
2024-10-17 09:08:41 +1300

    Add file to integrate hm with system

---
+++ system/nix/packages/home-manager.nix
@@ -0,0 +1,13 @@
+let
+  dotfilesDir = builtins.getEnv "DOTFILES_DIR";
+  user = builtins.getEnv "USER";
+  nixDir = "${dotfilesDir}/system/nix";
+in { config, pkgs, ... }:
+{
+  imports = [ <home-manager/nixos> ];
+
+  home-manager.users.${user} = {
+    imports = [ "${nixDir}/home/home.nix" ];
+  };
+  home-manager.useGlobalPkgs = true;
+}
