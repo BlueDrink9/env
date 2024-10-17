14e815d97dd4efa20ca917c991795e8d5fc51913
bluedrink9 <bluedrink9@users.noreply.github.com>
2024-10-18 11:51:15 +1300

    include make by defalut

--- system/nix/packages/main_nongui.nix
+++ system/nix/packages/main_nongui.nix
@@ -55,6 +55,7 @@
     # nix-du -s=500MB > store.dot; zgrviewer store.dot
     nix-du zgrviewer
     chatblade
+    make
 
 # so many programs rely on this for setup/install, so we'll put it in main.
     gcc
