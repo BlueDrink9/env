8faae095a528d6daa984a1cb7e775dd264b4f2db
bluedrink9 <bluedrink9@users.noreply.github.com>
2024-10-18 12:31:56 +1300

    Force hm stateversion to match nixos

--- system/nix/configuration.nix
+++ system/nix/configuration.nix
@@ -140,7 +140,7 @@ in { lib, config, pkgs, ... }:
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = lib.mkForce "24.05"; # Did you read the comment?
 
   # Perform garbage collection weekly to maintain low disk usage
   nix.gc = {
