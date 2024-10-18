5dda1724f78ceb354203a11ad95ed2311d3629f3
bluedrink9 <bluedrink9@users.noreply.github.com>
2024-10-18 16:01:19 +1300

    Fix nix-shel alias

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -224,6 +224,6 @@ alias del="gio trash"
 
 alias renix='sudo DOTFILES_DIR="$DOTFILES_DIR" nixos-rebuild switch'
 alias homer='home-manager switch'
-alias nix-shell="nix-shell --run=$SHELL"
+alias nix-shell="nix-shell --run $(basename $SHELL)"
 
 alias clip='xclip -selection clipboard'
