1fcadaa940c634b63f7789aa6a0c7e3c1f67a931
bluedrink9 <bluedrink9@users.noreply.github.com>
2024-10-18 15:19:54 +1300

    Set up rixi script to create r environments

--- system/nix/packages/R.nix
+++ system/nix/packages/R.nix
@@ -1,26 +1,47 @@
 { config, pkgs, ... }:
 {
 
-  environment.systemPackages = with pkgs; [
-    rWrapper.override{ packages = with rPackages; [
-      rix
+  environment.systemPackages = with pkgs;
+    let R = rWrapper.override{ packages = with rPackages; [
+      # rix
+      (pkgs.rPackages.buildRPackage {
+        name = "rix";
+        src = pkgs.fetchgit {
+          url = "https://github.com/ropensci/rix/";
+          rev = "287e8bd5d41649247747a499e459ef33cc7c76e0";
+          sha256 = "sha256-TaSzPijuk2EdmXfghd89mvNHpVgNqfYJsydt5/BPmGw=";
+        };
+        propagatedBuildInputs = builtins.attrValues {
+          inherit (pkgs.rPackages)
+          codetools
+          curl
+          jsonlite
+          sys;
+        };
+      })
       tidyverse
     ]; };
-    writeScriptBin "rixi" ''
-      ${pkgs.R}/bin/Rscript --cmd "
+      in [
+      R
+      (writeScriptBin "rixi" ''
       # This will create two files: .Rprofile and default.nix
-      rix::rix(
-        r_ver = \"4.4.1\",
-        r_pkgs = c($(cat requirements.txt) "dplyr", "ggplot2"),
-        system_pkgs = NULL,
-        git_pkgs = NULL,
-        ide = \"other\",
-        project_path = \".\",
-        overwrite = TRUE,
-        print = TRUE
-      )
-      "
-  ''
-  ];
+      ${R}/bin/R -e '
+        requirements <- c(
+          system2("cat", args=c("requirements.txt", "||", "echo"), stdout = TRUE, stderr = FALSE),
+          "dplyr",
+        )
+        rix::rix(
+          r_ver = "4.3.3",
+          r_pkgs = requirements,
+          system_pkgs = NULL,
+          git_pkgs = NULL,
+          ide = "other",
+          project_path = ".",
+          overwrite = TRUE,
+          print = TRUE
+        )
+      '
+      '')
+      ];
 
 }
