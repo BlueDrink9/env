0e6aaf81ef0fbe9407c61ef773e12ed49f344436
bluedrink9 <bluedrink9@users.noreply.github.com>
2024-10-21 09:19:06 +1300

    Set gx keybindings

--- editors/vim/runtimepath/lua/plugins/main.lua
+++ editors/vim/runtimepath/lua/plugins/main.lua
@@ -194,8 +194,13 @@ return {
 
 	{
 		"chrishrb/gx.nvim",
-		keys = { "gx" },
+		{ { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+		cmd = { "Browse" },
+		init = function ()
+			vim.g.netrw_nogx = 1 -- disable netrw gx
+		end,
 		dependencies = { "nvim-lua/plenary.nvim" },
+		submodules = false, -- not needed, submodules are required only for tests
 		opts = {
 			handler_options = {
 				search_engine = "duckduckgo", -- you can select between google, bing, duckduckgo, and ecosia
