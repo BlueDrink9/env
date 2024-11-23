3594009de4cf4ef6b5d77cd4ea83f7ae83218fae
BlueDrink9 <bluedrink9@users.noreply.github.com>
2024-11-24 11:05:29 +1300

    Nixos: fall back to homemanager tarball if not installed as module

--- nix/packages/home-manager.nix
+++ nix/packages/home-manager.nix
@@ -2,9 +2,21 @@ let
   dotfilesDir = builtins.getEnv "DOTFILES_DIR";
   user = builtins.getEnv "USER";
   nixDir = "${dotfilesDir}/system/nix";
+
+  homeManagerTarball = builtins.fetchTarball {
+    url = "https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz";
+    sha256 = "00wp0s9b5nm5rsbwpc1wzfrkyxxmqjwsc1kcibjdbfkh69arcpsn";
+  };
+
+  can_import = builtins.tryEval(import <home-manager/nixos>);
+  homeManagerPath = if can_import.success
+    then <home-manager/nixos>
+    else "${homeManagerTarball}/nixos";
+  hm = import homeManagerPath;
+
 in { config, pkgs, ... }:
 {
-  imports = [ <home-manager/nixos> ];
+  imports = [ hm ];
 
   # home-manager.users.${user} = {
   #   imports = [ "${nixDir}/home/home.nix" ];
