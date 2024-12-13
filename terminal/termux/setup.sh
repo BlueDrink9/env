1766a3c10a56957f632e22e0fdfbb6284f79ed59
bluedrink9 <bluedrink9@users.noreply.github.com>
2024-12-13 22:23:09 +1300

    Disable old unused termux stuff

--- terminal/termux/setup.sh
+++ terminal/termux/setup.sh
@@ -7,17 +7,18 @@ binDir="$HOME/.local/bin"
 mkdir -p "$binDir"
 
 mkdir -p "$HOME/.termux"
-downloadURLAndExtractZipTo "https://github.com/adi1090x/termux-style/archive/ee41ce0e1c261fb403763393293797fdc9784a52.zip" \
-    "$HOME/.termux/termux-style" && \
-    cp "$HOME/.termux/termux-style/solarized-light.properties" "$HOME/.termux/"
+# downloadURLAndExtractZipTo "https://github.com/adi1090x/termux-style/archive/ee41ce0e1c261fb403763393293797fdc9784a52.zip" \
+#     "$HOME/.termux/termux-style" && \
+#     cp "$HOME/.termux/termux-style/solarized-light.properties" "$HOME/.termux/"
 
 # Allow launching of apps by name?
 # Provide launcher support. launch _
-curl https://github.com/thewisenerd/dotfiles/blob/88d0534c67c2e93f601b1dd757cea9072eda6827/bin/zx -o "$binDir"/launch
+# curl https://github.com/thewisenerd/dotfiles/blob/88d0534c67c2e93f601b1dd757cea9072eda6827/bin/zx -o "$binDir"/launch
 
 # Disable openssh password auth. Only allow public key.
 sed -i 's/#PasswordAuthentication.*/PasswordAuthentication no/' "$PREFIX/etc/ssh/sshd_config"
 
+set -x
 # Set up fingerprint ssh auth. Need to have existing key already copied in.
 if ! termux-keystore list | grep -q '"alias": "default"'; then
   termux-keystore generate 'default' -a RSA -s 4096 -u 10
@@ -26,6 +27,7 @@ ln -fs "$($SCRIPTDIR_CMD)/termux-ssh-askpass" "$binDir"/termux-ssh-askpass
 # Interactive (requires typing in old passphrase). Changes it to new one.
 SSH_ASKPASS_REQUIRE="" SSH_ASKPASS="" ssh-keygen -p -f ~/.ssh/id_rsa \
     -N "$(sh "$binDir/termux-ssh-askpass" ~/.ssh/id_rsa)"
+set +x
 
 
 # Tasks are run in the background (termux isn't launched).
@@ -43,7 +45,7 @@ touch ~/.hushlogin
 
 # If this has already been done, it asks if you want to recreate.
 yes 'n' | termux-setup-storage > /dev/null
-ln -fs "$HOME/storage/shared/Work" "$HOME/Work"
+[ ! -d "$HOME/Work" ] && ln -fs "$HOME/storage/shared/Work" "$HOME/Work"
 
 chmod +x "$binDir"/*
 
