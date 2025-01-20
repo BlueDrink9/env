234fbbd4843fa52cee9138f6c9cd03777f33ad63
bluedrink9 <>
2025-01-21 11:25:44 +1300

    Fix home manager mimeapp syntax

--- nix/home/mime_apps.nix
+++ nix/home/mime_apps.nix
@@ -1,38 +1,40 @@
-
 { config, pkgs, lib, ... }:
 {
-  xdg.mime.defaultApplications = {
-    "text/plain" = [
-      "neovide.desktop"
-      "nvim.desktop"
-      "gvim.desktop"
-      "vim.desktop"
-    ];
-    "text/markdown" = [
-      "neovide.desktop"
-      "nvim.desktop"
-      "gvim.desktop"
-      "vim.desktop"
-    ];
-    "text/html" = [
-      "firefox.desktop"
-      "neovide.desktop"
-      "nvim.desktop"
-      "gvim.desktop"
-      "vim.desktop"
-    ];
-    "application/json" = "neovide.desktop";
-    "application/pdf" = "okular.desktop";
-    "image/png" = [
-      "gwenview.desktop"
-      "gimp.desktop"
-    ];
-    "image/svg+xml" = [
-      "firefox.desktop"
-      "inkscape.desktop"
-      "gwenview.desktop"
-    ];
-    "x-scheme-handler/http" = "firefox.desktop";
-    "x-scheme-handler/https" = "firefox.desktop";
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/plain" = [
+        "neovide.desktop"
+        "nvim.desktop"
+        "gvim.desktop"
+        "vim.desktop"
+      ];
+      "text/markdown" = [
+        "neovide.desktop"
+        "nvim.desktop"
+        "gvim.desktop"
+        "vim.desktop"
+      ];
+      "text/html" = [
+        "firefox.desktop"
+        "neovide.desktop"
+        "nvim.desktop"
+        "gvim.desktop"
+        "vim.desktop"
+      ];
+      "application/json" = "neovide.desktop";
+      "application/pdf" = "okular.desktop";
+      "image/png" = [
+        "gwenview.desktop"
+        "gimp.desktop"
+      ];
+      "image/svg+xml" = [
+        "firefox.desktop"
+        "inkscape.desktop"
+        "gwenview.desktop"
+      ];
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+    };
   };
 }
