1bc4d4a96cfbcd6adcb45287908b8471b2059e12
bluedrink9 <bluedrink9@github.com>
2025-01-26 13:02:40 +1300

    talon no hud

--- nix/home/talon.nix
+++ nix/home/talon.nix
@@ -12,11 +12,11 @@
 
     [ ! -d rango-talon ] && ${pkgs.git}/bin/git clone https://github.com/david-tejada/rango-talon
     [ ! -d talon-ai-tools ] && ${pkgs.git}/bin/git clone https://github.com/c-loftus/talon-ai-tools
-    [ ! -d talon_hud ] && ${pkgs.git}/bin/git clone https://github.com/chaosparrot/talon_hud
+    # [ ! -d talon_hud ] && ${pkgs.git}/bin/git clone https://github.com/chaosparrot/talon_hud
     [ ! -d cursorless-talon ] && ${pkgs.git}/bin/git clone https://github.com/cursorless-dev/cursorless-talon.git
     [ ! -d neovim-talon ] && ${pkgs.git}/bin/git clone https://github.com/hands-free-vim/neovim-talon.git
     [ ! -d talon-vim ] && ${pkgs.git}/bin/git clone https://github.com/fidgetingbits/talon-vim.git
-    ../bin/pip install pynvim
+    ../bin/pip install pynvim neovim
   '';
 
 }
