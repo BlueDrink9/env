b762ca377d18528242154609c9c80280adc86e51
bluedrink9 <bluedrink9@github.com>
2025-02-23 11:57:38 +1300

    fix mani sync

--- nix/home/talon.nix
+++ nix/home/talon.nix
@@ -18,12 +18,18 @@ let
   fonts.fontconfig.enable = true;
 
   home.activation.talon = ''
+    PATH=$PATH:${lib.makeBinPath [ pkgs.mani  pkgs.openssh ]}
+    # Non-nixos git ssh settings may be funky.
+    # https://github.com/NixOS/nixpkgs/issues/160527
+    if [ ! -d /etc/nixos ]; then
+      export GIT_SSH="/usr/bin/ssh"
+    fi
     export talon_mani_config="${../../talon/talon_plugins_mani.yml}"
     export talon_user_dir="$HOME/.talon/user"
-    ${mani}/bin/mani sync --parallel --config $talon_mani_config
-    ${mani}/bin/mani run update --parallel --config $talon_mani_config
+    mani sync --parallel --config $talon_mani_config
+    mani run update --parallel --config $talon_mani_config
+  '';
     # pushd ${../../talon}
     # ${gitman}/bin/gitman install
-  '';
 
 }
