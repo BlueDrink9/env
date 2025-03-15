20db3530997036ee1e5cdacc85fe24ac238c284f
bluedrink9 <bluedrink9@github.com>
2025-03-16 11:16:57 +1300

    talon: use local beta; install pynvim on activation

--- nix/home/talon.nix
+++ nix/home/talon.nix
@@ -1,21 +1,30 @@
 { lib, config, pkgs, ... }:
 let
     talon_pkg = (builtins.getFlake "github:nix-community/talon-nix").packages.${builtins.currentSystem}.default;
+  talon_beta = (
+      builtins.getFlake "git+file://${config.home.homeDirectory}/src/talon-nix"
+    ).packages.${builtins.currentSystem}.default;
+
   in
 {
 
   home.packages = with pkgs; [
-    talon_pkg
+    talon_beta
+    # talon_pkg
     mani
     # MS core fonts, needed for non-nixos linux?
     corefonts
-    (python3.withPackages (python-pkgs: with python-pkgs; [
-      pynvim
-      neovim
-      # gitman
-    ]))
+    # (python3.withPackages (python-pkgs: with python-pkgs; [
+    #   pynvim
+    #   neovim
+    #   # gitman
+    # ]))
   ];
   fonts.fontconfig.enable = true;
+
+  home.activation."talon python packages" = ''
+    ~/.talon/bin/python -m pip install pynvim
+  '';
 
   home.activation."talon user repos" = ''
     PATH=$PATH:${lib.makeBinPath [ pkgs.mani  pkgs.openssh ]}
