02262b8ca1a43cbcccbc426fe7d24cccd3a8d7f3
bluedrink9 <bluedrink9@users.noreply.github.com>
2025-06-07 11:48:57 +1200

    vr external monitor setup

---
+++ nix/packages/vr.nix
@@ -0,0 +1,61 @@
+
+{ config, pkgs, lib, ... }:
+
+with pkgs; let
+in
+  {
+
+  environment.systemPackages = with pkgs; [
+   unstable.wlx-overlay-s
+   unstable.opencomposite
+  # (GPUOffloadApp steam "steam")
+  ];
+
+  # programs.steam = {
+  #   enable = true;
+  #   remotePlay.openFirewall = true;
+  #   localNetworkGameTransfers.openFirewall = true;
+  # };
+
+  programs.immersed.enable = true;
+
+  # programs.alvr.enable = true;
+  # programs.alvr.openFirewall = true;
+  services.wivrn = {
+    enable = true;
+    openFirewall = true;
+    package = unstable.wivrn;
+    # Write information to /etc/xdg/openxr/1/active_runtime.json, VR applications
+    # will automatically read this and work with WiVRn (Note: This does not currently
+    # apply for games run in Valve's Proton)
+    defaultRuntime = true;
+    # Run WiVRn as a systemd service on startup
+    autoStart = false;
+    # Config for WiVRn (https://github.com/WiVRn/WiVRn/blob/master/docs/configuration.md)
+    config = {
+      enable = true;
+      json = {
+        # 1.0x foveation scaling
+        scale = 1.0;
+        # 100 Mb/s
+        bitrate = 100000000;
+        encoders = [
+          {
+            encoder = "vaapi";
+            codec = "h265";
+            # 1.0 x 1.0 scaling
+            width = 1.0;
+            height = 1.0;
+            offset_x = 0.0;
+            offset_y = 0.0;
+          }
+        ];
+      };
+    };
+  };
+  services.monado.enable = true;
+  programs.envision.enable = true;
+  programs.envision.openFirewall = true;
+  programs.envision.package = unstable.envision;
+
+}
