48d4d5a13613155bea3ac9105f7aacd75408c4b0
bluedrink9 <bluedrink9@github.com>
2026-01-13 19:48:29 +1300

    move tobii rules to file

--- nix/system-manager/modules/hardware/tobii5.nix
+++ nix/system-manager/modules/hardware/tobii5.nix
@@ -3,17 +3,27 @@
 
 {
   config = {
-    services.udev.extraRules = ''
-      # Tobii5
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="2104", ATTRS{idProduct}=="0127", GROUP="plugdev", TAG+="uaccess"
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="2104", ATTRS{idProduct}=="0118", GROUP="plugdev", TAG+="uaccess"
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="2104", ATTRS{idProduct}=="0106", GROUP="plugdev", TAG+="uaccess"
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="2104", ATTRS{idProduct}=="0128", GROUP="plugdev", TAG+="uaccess"
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="2104", ATTRS{idProduct}=="010a", GROUP="plugdev", TAG+="uaccess"
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="2104", ATTRS{idProduct}=="0102", GROUP="plugdev", TAG+="uaccess"
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="2104", ATTRS{idProduct}=="0313", GROUP="plugdev", TAG+="uaccess"
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="2104", ATTRS{idProduct}=="0318", GROUP="plugdev", TAG+="uaccess"
-    '';
+    environment = {
+      etc = {
+        "udev/rules.d/10-talon.rules".text = (builtins.readFile ./tobii5-udev.rules);
+      };
+    };
+
+    # "Activation" script to reload udev rules, in case sys-mgr has changed them.
+    systemd.services.reload-udev-rules = {
+      description = "Reload udev rules";
+      enable = true;
+      # Start when sysmgr acviates
+      wantedBy = [ "system-manager.target" ];
+      after = [ "systemd-udevd.service" ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+      };
+      script = ''
+        ${pkgs.systemd}/bin/udevadm control --reload-rules
+      '';
+    };
 
   };
 }
