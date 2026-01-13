2cdff36c2c7d108384612311f5f883e2ac5dfe89
bluedrink9 <bluedrink9@github.com>
2026-01-13 20:35:55 +1300

    sysmanr boot binarys and sudo options

--- nix/system-manager/modules/default.nix
+++ nix/system-manager/modules/default.nix
@@ -14,18 +14,58 @@
     # environment.sessionVariables = rec {
     # };
 
-    environment = {
-      systemPackages = with pkgs; [
+    environment.systemPackages =
+      with pkgs;
+      let
+        reboot_to_windows_script = (
+          writeShellScriptBin "reboot_to_windows" ''
+            sudo ${pkgs.systemd}/bin/bootctl set-oneshot auto-windows && reboot
+          ''
+        );
+
+      in
+      [
+
+        reboot_to_windows_script
+        (makeDesktopItem {
+          name = "Reboot to Windows";
+          desktopName = "Reboot to Windows";
+          exec = "${reboot_to_windows_script}/bin/reboot_to_windows";
+          icon = ./img/win10_coloured.ico;
+          terminal = true;
+        })
+
+        (pkgs.writeScriptBin "reset-tobii-hub" (
+          builtins.readFile ../../../shell/scripts/reset-tobii-hub.py
+        ))
+
+        efibootmgr
+        # set_next_boot
+        #writeShellScriptBin adds shebang, writeScriptBin doesn't. Testing which is better.
+        (writeShellScriptBin "reboot_to_firmware" ''
+          sudo ${pkgs.systemd}/bin/bootctl set-oneshot auto-reboot-to-firmware-setup && reboot
+        '')
+
       ];
-      etc = {
-        # "foo.conf".text = ''
-        #   launch_the_rockets = true
-        # '';
-      };
-    };
 
     systemd.services = {
     };
+
+    environment.etc."sudoers.d/system-manager-options".text = ''
+      # Check system manager bin dir, for pwless scripts run as just their bin name rather than full path.
+      Defaults secure_path="/run/system-manager/sw/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+    '';
+
+    # eg
+    # environment.etc."sudoers.d/pwless_scripts".text = ''
+    #   my_user ALL = (root) NOPASSWD: /run/system-manager/sw/bin/reset-tobii-hub
+    #   my_user ALL = (root) NOPASSWD: /run/system-manager/sw/bin/reboot_to_windows
+    #   my_user ALL = (root) NOPASSWD: /run/system-manager/sw/bin/reboot_to_firmware
+    # '';
+
+    environment.etc."profile.d/editor".text = ''
+      export VISUAL=${pkgs.neovim}/bin/nvim
+    '';
 
     # system.activationScripts.udev.text = ''
     #   #!${pkgs.runtimeShell}
